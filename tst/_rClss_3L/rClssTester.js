0327eb26411d960439bc0e9bc05e43d6d1837352
clif edwards <clif.edwards.2@gmail.com>
2016-07-01 15:24:54 -0400

    * _rClss_3L/tests.js  * 160701  *  @1525 BROKEN - tests 3-5 break because there is no function YET to recompute the three lists  *  @1500 STABLE and TESTS requiring(crnt_limits.js)  *  @1349 using requires for current rClass beg and end.  *  @1055  trying to test main but do not know how to apply test to main

--- tst/_rClss_3L/rClssTester.js
+++ tst/_rClss_3L/rClssTester.js
@@ -1,33 +1,40 @@
 /**
  * _rClss_3L/tests.js
  * 160701
+ *  @1525 BROKEN - tests 3-5 break because there is no function YET to recompute the three lists
  *  @1500 STABLE and TESTS requiring(crnt_limits.js)
  *  @1349 using requires for current rClass beg and end.
  *  @1055  trying to test main but do not know how to apply test to main
  */
 
+var crnt  = require('./crnt_limits.js');
 var test = require('tape');
 var R = require('ramda');
-var crnt  = require('./crnt_limits.js');
+
 // test data
 var stubList = [0, 1, 2, 3, 4, 5, 6, 7, 8];// pretend these are verse INDEXES
 var stubVerses = stubList;
-var stub_crntLimits =  {beg: 3, end: 4};
-var stub_crntBeg = crnt._beg(stub_crntLimits);
-var stub_crntEnd = crnt._end(stub_crntLimits);
-var stub_crntSpan = crnt._span(stub_crntLimits);
+var stub_cL =  {beg: 3, end: 4};// stub_crntLimits`
 // test code
 
-var CUT = R.splitAt(stub_crntBeg)( stubVerses);
+var CUT = R.splitAt(crnt._beg(stub_cL))( stubVerses);
 var pst = CUT[0];
-test.pst = pst;
-CUT = R.splitAt(stub_crntSpan)(CUT[1]);
+CUT = R.splitAt(crnt._span(stub_cL))(CUT[1]);
 var cur = CUT[0];
 var fut = CUT[1];
 var noop;
-test('exp pst:3, cur:1, fut:5', function (t) {
-    t.equal(pst.length, stub_crntBeg);
-    t.equal(cur.length, 1);
-    t.equal(fut.length, 5);
+
+test('exp pst,cur,end for{beg:3, end:4}', function (t) {
+    stub_cL =  {beg: 3, end: 4};
+    t.equal(pst.length, crnt._beg(stub_cL));
+    t.equal(cur.length, crnt._span(stub_cL));
+    t.equal(fut.length, stubList.length - crnt._end(stub_cL) );
+    t.end();
+});
+test('exp pst,cur,end for{beg:6, end:8}', function (t) {
+    stub_cL =  {beg: 6, end: 8};
+    t.equal(pst.length, crnt._beg(stub_cL));
+    t.equal(cur.length, crnt._span(stub_cL));
+    t.equal(fut.length, stubList.length - crnt._end(stub_cL) );
     t.end();
 });
