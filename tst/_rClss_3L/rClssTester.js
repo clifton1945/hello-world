74ea20e6c61bc3d1c5c0729e93599e229a2ea728
clif edwards <clif.edwards.2@gmail.com>
2016-07-01 22:37:40 -0400

    * _rClss_3L/tests.js  * 160701  *  @2237 FIX _rClss_Lsts() & R.append and  *  @1706 new _rCls_Lsts()   STABLE & TESTED  *  @1525 NOTBROKE - tests 3-5 break because there is no function YET to recompute the three lists

--- tst/_rClss_3L/rClssTester.js
+++ tst/_rClss_3L/rClssTester.js
@@ -1,6 +1,7 @@
 /**
  * _rClss_3L/tests.js
  * 160701
+ *  @2237 FIX _rClss_Lsts() & R.append and
  *  @1706 new _rCls_Lsts()   STABLE & TESTED
  *  @1525 NOTBROKE - tests 3-5 break because there is no function YET to recompute the three lists
  *  @1500 STABLE and TESTS requiring(crnt_limits.js)
@@ -15,36 +16,62 @@ var R = require('ramda');
 // test data
 var stubList = [0, 1, 2, 3, 4, 5, 6, 7, 8];// pretend these are verse INDEXES
 var stubVerses = stubList;
-var stub_cL = {beg: 3, end: 4};// stub_crntLimits`
+var RET, EXP, CUT;
 // test code
 var _rCls_Lsts = chptVerses => crntLmts => {
-    var RET = [];
-    var CUT = R.splitAt(crnt._beg(crntLmts))(chptVerses);
-    var pst = R.flatten(CUT[0]) ;
-    var cf = R.flatten(CUT[1]) ;
-    CUT = R.splitAt(crnt._span(crntLmts))(cf);
-    var cur = R.flatten(CUT[0]);
-    var fut =R.flatten(CUT[1]);
+    RET = [];
+    var CUT1 = R.splitAt(crnt._beg(crntLmts))(chptVerses);
+    var pst = R.flatten(CUT1[0]);
+    var cf = R.flatten(CUT1[1]);
+    var CUT2 = R.splitAt(crnt._span(crntLmts))(cf);
+    var cur = R.flatten(CUT2[0]);
+    var fut = R.flatten(CUT2[1]);
     RET = R.append(pst)(RET); //pst
     RET = R.append(cur, RET);
     RET = R.append(fut, RET);
     return RET
 };
-var noop;
-
-test('exp pst,cur,end for{beg:3, end:4}', function (t) {
-    stub_cL = {beg: 3, end: 4};
-    RET = _rCls_Lsts(stubVerses)(stub_cL);
-    t.equal(RET[0].length, crnt._beg(stub_cL));
-    t.equal(RET[1].length, crnt._span(stub_cL));
-    t.equal(RET[2].length, stubList.length - crnt._end(stub_cL));
+    CUT = {beg: 111, end: 222};
+test(`exp _rClss_Lsts lengths: unflattened:3, flattened:9 `, function (t) {
+    RET = _rCls_Lsts(stubVerses)(CUT);
+    t.equal(RET.length, 3, 'w/o flattening exp 3:pst.cur,fut');
+    RET = R.flatten(RET);
+    t.equal(RET.length, 9, 'w/ flattening exp all 9 verses.');
     t.end();
 });
-test('exp pst,cur,end for{beg:6, end:8}', function (t) {
-    stub_cL = {beg: 6, end: 8};
-    RET = _rCls_Lsts(stubVerses)(stub_cL);
-    t.equal(RET[0].length, crnt._beg(stub_cL));
-    t.equal(RET[1].length, crnt._span(stub_cL));
-    t.equal(RET[2].length, stubList.length - crnt._end(stub_cL));
+    CUT = {beg: 0, end: 2};
+test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
+    RET = _rCls_Lsts(stubVerses)(CUT);
+    EXP = crnt._beg(CUT);
+    // t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
+    t.equal(R.flatten(RET[0]).length, 0, ` > pst:${EXP}`);
+    EXP = crnt._span(CUT);
+    // t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
+    t.equal(R.flatten(RET[1]).length, 2, ` > cur:${EXP}`);
+    EXP = stubList.length - crnt._end(CUT);
+    // t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
+    t.equal(R.flatten(RET[2]).length, 7, ` > fut:${EXP}`);
+    t.end();
+});
+    CUT = {beg: 3, end: 6};
+test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
+    RET = _rCls_Lsts(stubVerses)(CUT);
+    EXP = crnt._beg(CUT);
+    t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
+    EXP = crnt._span(CUT);
+    t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
+    EXP = stubList.length - crnt._end(CUT);
+    t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
+    t.end();
+});
+    CUT = {beg: 7, end: 9};
+test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
+    RET = _rCls_Lsts(stubVerses)(CUT);
+    EXP = crnt._beg(CUT);
+    t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
+    EXP = crnt._span(CUT);
+    t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
+    EXP = stubList.length - crnt._end(CUT);
+    t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
     t.end();
 });
