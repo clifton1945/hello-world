5dfed05166ffdde4906afb22ff8ba047783831b5
clif edwards <clif.edwards.2@gmail.com>
2016-07-04 11:15:15 -0400

    *  _rClss_Wter/main.js  *  160704  *  @1115   ADDED new structure: \src && \fut AND TEST  f_n_Wter is STABLE

--- style_Rclasses/main.js
+++ style_Rclasses/main.js
@@ -1,5 +1,7 @@
 /**
- *  _rClss_Wter/main.js TODO  reinvoke watchify for new class names
+ *  _rClss_Wter/main.js
+ *  160704
+ *  @1115   ADDED new structure: \src && \fut AND TEST  f_n_Wter is STABLE
  *  160702
  *      @2141 REFACTED names of weighter file and require in main. Now all use f_n_Wter.
  *      @2122 ADDED var nWt_ = require('./f_n_a_Wter.js') STABLE WORKS
@@ -9,16 +11,15 @@
  */
 var R = require('ramda');
 
-
 /**
  *      requires( the equivalent of f_n_Wter() FROM './f_n_Wter_'
  * @type {f_n_Wter_}
  * @private
  */
-var f_n_Wter_ = require('./tst/f_n_Wter_');
+var Wter = require('./src/f_n_Wter_');
 
 var test = require('tape');
 test("*********** _n_Wter_('fut')->", function (t) {
-    t.equals(f_n_Wter_('fut')([0, 1, 3], 0), 1);
+    t.equals(Wter.f_n_Wter_('fut')([0, 1, 3], 0), 1);
     t.end();
 });
