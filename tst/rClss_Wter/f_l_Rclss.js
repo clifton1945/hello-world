268123c4cb10224cb7cb12548a3134be4166ed50
clif edwards <clif.edwards.2@gmail.com>
2016-07-04 07:54:09 -0400

    * f_l_Rclss.js  -> [[pst],[cur],[fut]]  * 160704  *  @0752 FIXED f_n_Rclss()   STABLE && TESTED  *  @0549 REFACT f_n_Rclss NOT BROKE for now

--- tst/rClss_Wter/f_l_Rclss.js
+++ tst/rClss_Wter/f_l_Rclss.js
@@ -1,19 +1,15 @@
 /**
  * f_l_Rclss.js  -> [[pst],[cur],[fut]]
  * 160704
- *  @0549 REFACT f_n_Rclss BROKEN for now
+ *  @0752 FIXED f_n_Rclss()   STABLE && TESTED
+ *  @0549 REFACT f_n_Rclss NOT BROKE for now
  *  @0525 COPY OF _rClss_3L.js
  */
 
-var crnt = require('./crnt_limits.js');
+// var crnt = require('./crnt_limits.js');
 var test = require('tape');
 var R = require('ramda');
 
-// test data
-var stubList = [0, 1, 2, 3, 4, 5, 6, 7, 8];// pretend these are verse INDEXES
-var stubVerses = stubList;
-var RET, EXP, CUT;
-// test code
 /**
  *      f_n_Rclass:: L->D->L:[[pst],[cur],[pst]] a L of the current chapters three Rclss verses.
  * @param chptVerses
@@ -22,59 +18,60 @@ var RET, EXP, CUT;
  */
 var f_n_Rclass = (chptVerses, crntLmts) => {
     RET = [];
-    var CUT1 = R.splitAt(crnt._end(crntLmts))(chptVerses);
-    var fut = R.flatten(CUT1[1]);
-    var cf = R.flatten(CUT1[0]);
-    var CUT2 = R.splitAt(crnt._beg(crntLmts))(cf);
-    var cur = R.flatten(CUT2[1]);
-    var pst = R.flatten(CUT2[0]);
-    RET = R.append(pst)(RET); //pst
-    RET = R.append(cur, RET);
-    RET = R.append(fut, RET);
-    return RET
+    var CUT1 = R.splitAt(crntLmts.end)(chptVerses);
+    var fut = CUT1[1];
+    var CUT2 = R.splitAt(crntLmts.beg)(CUT1[0]);
+    var cur = CUT2[1];
+    var pst = CUT2[0];
+    return [pst,cur,fut]
 };
-CUT = {beg: 111, end: 222};
-test(`exp _rClss_Lsts lengths: unflattened:3, flattened:9 `, function (t) {
-    RET = f_n_Rclass(stubVerses)(CUT);
-    t.equal(RET.length, 3, 'w/o flattening exp 3:pst.cur,fut');
-    RET = R.flatten(RET);
-    t.equal(RET.length, 9, 'w/ flattening exp all 9 verses.');
-    t.end();
-});
-CUT = {beg: 0, end: 2};
-test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
-    RET = f_n_Rclass(stubVerses)(CUT);
-    EXP = crnt._beg(CUT);
-    // t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
-    t.equal(R.flatten(RET[0]).length, 0, ` > pst:${EXP}`);
-    EXP = crnt._span(CUT);
-    // t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
-    t.equal(R.flatten(RET[1]).length, 2, ` > cur:${EXP}`);
-    EXP = stubList.length - crnt._end(CUT);
-    // t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
-    t.equal(R.flatten(RET[2]).length, 7, ` > fut:${EXP}`);
-    t.end();
-});
-CUT = {beg: 3, end: 6};
-test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
-    RET = f_n_Rclass(stubVerses)(CUT);
-    EXP = crnt._beg(CUT);
-    t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
-    EXP = crnt._span(CUT);
-    t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
-    EXP = stubList.length - crnt._end(CUT);
-    t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
-    t.end();
-});
-CUT = {beg: 7, end: 9};
-test(`EXP pst,cur,end FOR ${JSON.stringify(CUT)}`, function (t) {
-    RET = f_n_Rclass(stubVerses)(CUT);
-    EXP = crnt._beg(CUT);
-    t.equal(R.flatten(RET[0]).length, EXP, ` > pst:${EXP}`);
-    EXP = crnt._span(CUT);
-    t.equal(R.flatten(RET[1]).length, EXP, ` > cur:${EXP}`);
-    EXP = stubList.length - crnt._end(CUT);
-    t.equal(R.flatten(RET[2]).length, EXP, ` > fut:${EXP}`);
-    t.end();
-});
 
+// test data
+var stubList = [0, 1, 2, 3, 4, 5, 6, 7, 8];// pretend these are verse INDEXES
+var stubVerses = stubList;
+var RET, EXP, CUT, limits, SKIP={skip:false};
+// test code:
+
+test(`exp f_n_Rclass.length -> 3 un flattened, 9 flattened.`, SKIP,
+    function (t) {
+    RET = f_n_Rclass(stubVerses, {beg: 111, end: 222});
+    t.equal(RET.length, 3, 'w/o flattening exp 3 lists of verses:pst.cur,fut');
+    t.equal(R.flatten(RET).length, 9, 'w/ flattening exp all 9 verses.');
+    t.end();
+});
+// limits = {beg: 0, end: 2};
+test(`EXP pst,cur,end FOR ${JSON.stringify({beg: 0, end: 2})}`,
+    function (t) {
+    RET = f_n_Rclass(stubVerses, {beg: 0, end: 2});
+    EXP = 0;
+    t.equal(RET[0].length, EXP, ` > pst:${EXP}`);
+    EXP = 2;
+    t.equal(RET[1].length, EXP, ` > cur:${EXP}`);
+    EXP = 7;
+    t.equal(RET[2].length, EXP, ` > fut:${EXP}`);
+    t.end();
+});
+limits = {beg: 3, end: 6};
+test(`EXP pst,cur,end FOR ${JSON.stringify({beg: 3, end: 6})}`,
+    function (t) {
+        RET = f_n_Rclass(stubVerses, {beg: 3, end: 6});
+        EXP = 3;
+        t.equal(RET[0].length, EXP, ` > pst:${EXP}`);
+        EXP = 3;
+        t.equal(RET[1].length, EXP, ` > cur:${EXP}`);
+        EXP = 3;
+        t.equal(RET[2].length, EXP, ` > fut:${EXP}`);
+        t.end();
+    });
+limits = {beg: 7, end: 9};
+test(`EXP pst,cur,end FOR ${JSON.stringify({beg: 7, end: 9})}`,
+    function (t) {
+        RET = f_n_Rclass(stubVerses, {beg: 7, end: 9});
+        EXP = 7;
+        t.equal(RET[0].length, EXP, ` > pst:${EXP}`);
+        EXP = 2;
+        t.equal(RET[1].length, EXP, ` > cur:${EXP}`);
+        EXP = 0;
+        t.equal(RET[2].length, EXP, ` > fut:${EXP}`);
+        t.end();
+    });
