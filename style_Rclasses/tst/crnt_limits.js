920323b1d505154d34ae121f2e3fa7a07733099e
clif edwards <clif.edwards.2@gmail.com>
2016-07-05 08:31:17 -0400

    * crntLimits.js (D:rClss:cur limits:beg, end) -> ()-> {}  *  160705  *  @0830 MOVED file FROM root  TO \tst\

--- style_Rclasses/tst/crnt_limits.js
+++ style_Rclasses/tst/crnt_limits.js
@@ -1,9 +1,12 @@
 /**
  * crntLimits.js (D:rClss:cur limits:beg, end) -> ()-> {}
+ *  160705
+ *  @0830 MOVED file FROM root  TO \tst\
  * 160701
  *  @1442 partial conversion to ramda
  */
 var R = require('ramda');
+
 module.exports = {
     _beg: R.prop('beg'), // D -> {D -> N}
     _end: R.prop('end'), // D -> {D -> N}
