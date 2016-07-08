836f2413215d4e4c04dfa0b3d0b01565abc4a197
clif edwards <clif.edwards.2@gmail.com>
2016-07-08 10:02:09 -0400

    *  d_Chpt_curScope.js  *  160708 @ 1000  CREATED a constant fixed dictionary FOR  *   defining the beg and end indexes OF the Rclss.current category.

---
+++ Chptr_2_Rclss/src/d_Chptr_curScope.js
@@ -0,0 +1,6 @@
+/**
+ *  d_Chpt_curScope.js
+ *  160708 @ 1000  CREATED a constant fixed dictionary FOR
+ *   defining the beg and end indexes OF the Rclss.current category.
+ */
+module.exports = {beg: 0, end: 1}; // default
