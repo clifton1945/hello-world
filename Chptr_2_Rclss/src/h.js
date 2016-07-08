55c0103fe88b6eef699811bc94ac4c3e59692460
clif edwards <clif.edwards.2@gmail.com>
2016-07-08 08:08:47 -0400

    * f_d_curChptScope.js @0805 MOVED f_d_set_key TO h.js FROM here

--- Chptr_2_Rclss/src/h.js
+++ Chptr_2_Rclss/src/h.js
@@ -1,10 +1,24 @@
 /**
- * h.js  Created by CLIF on 7/6/2016.\
+ * h.js 160708 @ 0805  MOVED f_d_set_key TO here FROM f_d_Chpt_curScope.js
+ * Created by CLIF on 7/6/2016.\
  * helper files
  */
 
 var R = require('ramda');
 var test = require('tape');
+
+/**
+ *      f_d_set_key:: S:key -> D:{key:val} -> N:val -> D:{key:val}
+ *      USAGE: setting d_curScope at the Chptr level.
+ *      TESTS in tst/f_d_Chptr_curScope.js
+ * @param key
+ * @param dict
+ * @param val
+ */
+const _d_set_key = R.curry(
+// const f_d_set_key = R.curry(
+    (key, dict, val)=> R.set(R.lensProp(key), val, dict)
+);
 
 var ltBeg = (dict)=>(i)=> R.lt(i, dict.begNdx);//D -> N -> Bool
 var gtEnd = (dict)=>(i)=> R.gt(i, dict.endNdx);//D -> N -> Bool
@@ -17,7 +31,8 @@ var isPst = (dict) => ltBeg(dict); //: D:scope->Nlt:i -> Bool
 var isFut = (dict) => gtEnd(dict); //: N:i -> Bool
 var isCur = (dict) => tweenBegEnd(dict);
 
-module.exports = {isPst, isCur, isFut};
+module.exports = {_d_set_key};
+// module.exports = {f_d_set_key};
 //
 // test = ("use es6 unpack ", function (t) {
 //     var b, e;
