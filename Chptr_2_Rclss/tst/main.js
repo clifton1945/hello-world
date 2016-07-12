ac5a4833cc6b31f6c6a77ba7eacb24806004825c
clif edwards <clif.edwards.2@gmail.com>
2016-07-12 08:19:26 -0400

    *  main.js IN Chptr_2_Rclss/.../tst/  *  160712 -> @0817 CONFIRMED getComputeredSTyle IS a great base for transformer functions.

--- Chptr_2_Rclss/tst/main.js
+++ Chptr_2_Rclss/tst/main.js
@@ -1,6 +1,6 @@
 /**
  *  main.js IN Chptr_2_Rclss/.../tst/
- *  160712 -> BEGIN ADDING transforms
+ *  160712 -> @0817 CONFIRMED getComputeredSTyle IS a great base for transformer functions.
  *      @0617 holy smokes THIS WORKS SO WELL. Added transform: wt_fontSize_px
  *  -> getComputedStyles WORKS!!
  *      @0554 CSDs from getComputedStyles ARE RECOGNIZED BY evolve
@@ -28,6 +28,7 @@ var C_Both = require('../src/h').C_Both;
 var n_Scale = .75;
 var wt_opacity = R.compose(R.toString, R.multiply(n_Scale), parseFloat);
 var wt_fontSize_px = R.compose(R.flip(R.concat)('px'), R.toString, R.multiply(n_Scale), parseFloat);
+var wt_fontSize_Prcnt = R.compose(R.flip(R.concat)('%'), R.toString, R.multiply(n_Scale), parseFloat);
 
 var test = require('tape');
 //GLOBALS
@@ -46,8 +47,6 @@ var trnsfrms = {
 // MAIN CodeUnderTest
 CUT = R.evolve(trnsfrms);
 
-
-
 test('0 ***** main: evolve a hard coded CSD', function (t) {
     t_csd = {id: 0, fontSize: '100%', opacity: '1.0', textAlign: 'center'};
     var RET = R.evolve(trnsfrms)(t_csd);
@@ -56,10 +55,9 @@ test('0 ***** main: evolve a hard coded CSD', function (t) {
 });
 test('1 ***** main: see a testDoc.html Elem', function (t) {
     var e_aVerse = nl_allVerses.item(2);
-    var RET = e_aVerse;
     C_Both(JSON.stringify(nl_allVerses.length ) + ' length.');// -> "" no opacity set yet LATER: look for the CSS set  opacity
     C_Both(JSON.stringify(t_csd) + ' default.');// -> "" no opacity set yet LATER: look for the CSS set  opacity
-    t.equals(RET.innerText, 'chptr:1 verse:3 ndx:2', 'this Elem.innerText');
+    t.equals(e_aVerse.innerText, 'chptr:1 verse:3 ndx:2', 'this Elem.innerText');
     t.end();
 });
 test('2 ***** main: evolve a testDoc.html CSD', function (t) {
@@ -74,29 +72,52 @@ test('2 ***** main: evolve a testDoc.html CSD', function (t) {
 });
 test('3 ***** main: getComputerStyles ', function (t) {
     var e_aVerse = nl_allVerses.item(2);
-    var computerStyle = window.getComputedStyle(e_aVerse);
-    t.equals( R.isEmpty(computerStyle.backgroundColor), false,"backgroundColor is NOT Empty.");
-    t.equals(computerStyle.backgroundColor, 'rgba(0, 0, 0, 0)', 'backgroundColor -> "rgba(0, 0, 0, 0)".');
+    var computedStyle = window.getComputedStyle(e_aVerse);
+    t.equals( R.isEmpty(computedStyle.backgroundColor), false,"backgroundColor is NOT Empty.");
+    t.equals(computedStyle.backgroundColor, 'rgba(0, 0, 0, 0)', 'backgroundColor -> "rgba(0, 0, 0, 0)".');
     t.end();
 });
 test('4 ***** main: evolve a getComputerStyles.opacity ', function (t) {
     var e_aVerse = nl_allVerses.item(2);
-    var computerStyle = window.getComputedStyle(e_aVerse);
-    t.equals( R.isEmpty(computerStyle.opacity), false,"opacity is NOT Empty.");
-    t.equals(computerStyle.opacity, '1', 'opacity -> "1".');
-    computerStyle = R.evolve(trnsfrms)(computerStyle);//-> NOW evolve works!
-    t.deepEquals(computerStyle.opacity, '0.75', 'evolved opacity -> 0.75');
+    var computedStyle = window.getComputedStyle(e_aVerse);
+    t.equals( R.isEmpty(computedStyle.opacity), false,"opacity is NOT Empty.");
+    t.equals(computedStyle.opacity, '1', 'opacity -> "1".');
+    computedStyle = R.evolve(trnsfrms)(computedStyle);//-> NOW evolve works!
+    t.deepEquals(computedStyle.opacity, '0.75', 'evolved opacity -> 0.75');
     t.end();
 });
 test('5 ***** main: evolve a getComputerStyles.fontSize ', function (t) {
     var e_aVerse = nl_allVerses.item(1);
-    // var computerStyle = window.getComputedStyle(e_aVerse);
-    var computerStyle = getComputedStyle(document.body, null);
-    t.equals( R.isEmpty(computerStyle.fontSize), false,"fontSize is NOT Empty.");
-    t.equals(computerStyle.fontSize, '20px', 'fontSize -> "20px".');
-    computerStyle = R.evolve(trnsfrms)(computerStyle);//-> evolve USED wt_fontSize_px!
-    t.deepEquals(computerStyle.fontSize, '15px', 'evolved fontSize -> 15px');
-    C_Both(JSON.stringify(computerStyle.fontSize));
-
+    var computedStyle = getComputedStyle(document.body, null);
+    t.equals( R.isEmpty(computedStyle.fontSize), false,"fontSize is NOT Empty.");
+    t.equals(computedStyle.fontSize, '20px', 'fontSize -> "20px".');
+    computedStyle = R.evolve(trnsfrms)(computedStyle);//-> evolve USED wt_fontSize_px!
+    t.deepEquals(computedStyle.fontSize, '15px', 'evolved fontSize -> 15px');
+    t.end();
+});
+test('6 ***** main: assure I SEE <style>->#tstVerse{font-size: 30px;', function (t) {
+    var e_aVerse = document.querySelector('#tstVerse');//->'chptr:2 verse:6 ndx:5'
+    t.equals(e_aVerse.innerText, 'chptr:2 verse:6 ndx:5', 'tstVerse');
+    var computedStyle = window.getComputedStyle(e_aVerse);
+    // var computedStyle = getComputedStyle(document.body, null);// WATCH OUT not same as window.getComputedStyle(e_aVerse)
+    C_Both(JSON.stringify(computedStyle.fontSize));
+    t.equals( R.isEmpty(computedStyle.fontSize), false,"fontSize is NOT Empty.");
+    t.equals(computedStyle.fontSize, '30px', 'styled fontSize -> "30px".');
+    computedStyle = R.evolve(trnsfrms)(computedStyle);//-> evolve USED wt_fontSize_px!
+    t.deepEquals(computedStyle.fontSize, '22.5px', 'evolved fontSize -> 22.5px');
+    C_Both(JSON.stringify(computedStyle.fontSize));
+    t.end();
+});
+test('7 ***** main: evolve a <style>->#tstVerse{font-size: 30px;', function (t) {
+    var e_aVerse = document.querySelector('#tstVerse');//->'chptr:2 verse:6 ndx:5'
+    t.equals(e_aVerse.innerText, 'chptr:2 verse:6 ndx:5', 'tstVerse');
+    var computedStyle = window.getComputedStyle(e_aVerse);
+    t.equals(computedStyle.fontSize, '30px', 'styled fontSize -> "30px".');
+    var n_Scale = 0.5;
+    wt_fontSize_px = R.compose(R.flip(R.concat)('px'), R.toString, R.multiply(n_Scale), parseFloat);
+    trnsfrms.fontSize = wt_fontSize_px;
+    computedStyle = R.evolve(trnsfrms)(computedStyle);//-> evolve USED wt_fontSize_px!
+    t.deepEquals(computedStyle.fontSize, '15px', 'evolved fontSize -> 15px');
+    C_Both(JSON.stringify(computedStyle.fontSize));
     t.end();
 });
