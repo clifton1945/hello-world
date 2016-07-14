e970de43007e937a2a4671d32176fb23eb4646bf
clif edwards <clif.edwards.2@gmail.com>
2016-07-14 17:11:11 -0400

    *  f_d_evolveTrnsfrms.js  * 160714 @1710 ->  STILL DO NOT KNOW HOW TO partial a trandformation  *  @1311 stable test("0  *****  R.partial(_scaleN) -> _ndxd_scaleN  *****"  *  @1029 -> NEW f_d_evolveTrnsfrms branched from f_d_transforms.js branched from f_evolve_Trnsfrms.js

--- Chptr_2_Rclss/tst/f_d_evolveTrnsfrms.js
+++ Chptr_2_Rclss/tst/f_d_evolveTrnsfrms.js
@@ -1,61 +1,46 @@
 /**
  *  f_d_evolveTrnsfrms.js
- * 160714 @1311 stable test("0  *****  R.partial(_scaleN) -> _ndxd_scaleN  *****"
+ * 160714 @1710 ->  STILL DO NOT KNOW HOW TO partial a trandformation
+ *  @1311 stable test("0  *****  R.partial(_scaleN) -> _ndxd_scaleN  *****"
  *  @1029 -> NEW f_d_evolveTrnsfrms branched from f_d_transforms.js branched from f_evolve_Trnsfrms.js
  */
 "use strict";
 let R = require('ramda');
-var stubScale = R.identity(.5); // why not just use .5????
-var stubOffset = R.identity(-0.6);
-// these are the base transform functions named for convenience:
-// fix maybe should use R.partial on the 3 below
-const _setScale = f => f(stubScale);
-const _set = (f) => (n) => f(n);
-const xD = {
-    _scaleN: (f) => (n) => f(n),
-    _offsetN: (f) => (n) => f(n),
-};
 // these are my base transform functions named for convenience:
 const _scaleN = R.multiply;// N -> N -> N
 const _offsetN = R.add; // N -> N -> N
-// ---------------------- Code Under Test: f_evolve_Trnsfrms
-const transformsD = {
-    scale: _scaleN,
-    offset: _offsetN
-};
-
-/**
- *      f_evolve_Trnsfrms:: F:{k:{v->v}} -> D:{k:v} -> D:{k:v}
- *      D:{k:v}transformsD, D:dCSD, N:nScale -> D:dCSD
- *  scales CSD properties hardcoded into this D:: transformsD
- *  some configurations of this might include:
- *      transformsD as parameter,
- * @param dCSD ->
- * @return dCSD
- * @param transformsD
- */
-// const f_evolve_Trnsfrms = function f_evolve_Trnsfrms(transformsD, dCSD) {
-//     return R.evolve(_set(.5), transformsD); //F:{k:{v->v}} -> D:{k:v} -> D:{k:v}
-// };
-
 
 // ---------------------- test: f_evolve_Trnsfrms
 let test = require('tape');
 test("0  *****  R.partial(_scaleN) :f_evolve_Trnsfrms *****", function (t) {
-    // var _ndxd_scaleN = R.partial(_scaleN, [.5]); // preload/partial
     var _ndxd_scaleN = R.partial(_scaleN); // preload/partial
-    t.deepEquals(_ndxd_scaleN([.5])('1'), .5, " _ndxd_scaleN:'1'->'0.5'");
+    var passedInPropValu = '1';
+    t.deepEquals(_ndxd_scaleN([.5])(passedInPropValu), .5, " _ndxd_scaleN:'1'->'0.5'");
     t.end();
 });
-// test("1  *****  R.evolve(_scaleN) :f_evolve_Trnsfrms *****", function (t) {
-//     var trnsfrms4_CsdD = {scale: _scaleN, offset: _offsetN}; //
-//     var trnsfrms4_Trnsfrms = {_scaleN: };
-//     var _ndxd_scaleN = R.partial(_scaleN)([.5]); // preload/partial
-//     var CUT = R.evolve(()=> {
-//     })(transformsD);
-//     t.deepEquals(_ndxd_scaleN('1'), .5, " _ndxd_scaleN:'1'->'0.5'");
-//     t.end();
-// });
+test("1  *****  revolve(_scaleN) the F:_scaleN :f_evolve_Trnsfrms *****", function (t) {
+    var scale_CsdD = {scale: _scaleN}; //
+    // var scale_CsdD_Trnsfrms = {scale: R.tap(_scaleN)};
+    var revolve = R.flip(R.evolve);// NOTE: FLIPPED evolve::(trgtDict, trgtTrnsfrm) -> trgtDict
+    var CUT = revolve(scale_CsdD)( {scale: R.tap(_scaleN)}); // exp return the orig _scaleN:
+    t.deepEquals(CUT.scale(1)(1), 1, " scale: tap 1 -> 1");
+    // CUT = revolve(scale_CsdD)({scale: R.partial([.5])}); // BROKEN exp return the orig _scaleN:
+    CUT = revolve(scale_CsdD)({scale: R.flip(R.partialRight)([.5])}); // exp .5 * 2nd param:
+    t.deepEquals(CUT.scale(1), 0.5, " scale:R.flip(R.partialRight)[.5]1 -> 0.5");
+    t.end();
+});
+test("2  *****  revolve(new_scaleN) the F:_scaleN :f_evolve_Trnsfrms *****", function (t) {
+    var new_scaleN = R.curry(function new_scaleN (wt){ return R.multiply(wt)});
+    var scale_CsdD = {scale: new_scaleN}; //
+    // var scale_CsdD_Trnsfrms = {scale: R.tap(_scaleN)};
+    var revolve = R.flip(R.evolve);// NOTE: FLIPPED evolve::param NOW (trgtDict, trgtTrnsfrm) -> trgtDict
+    // var newDict = revolve(scale_CsdD)( {scale: R.tap(new_scaleN)}); // exp return the orig func
+    // t.deepEquals(newDict.scale(0.8)(1), 0.8, " scale: tap 1 -> 0.8");
+
+    var newDict = revolve(scale_CsdD)({scale: R.flip(R.partialRight)([.5])}); // exp .5 * 2nd param:
+    t.deepEquals(newDict.scale(.8), 0.4, "BROKEN ???  1 -> 0.5");
+    t.end();
+});
 // --------------------- ON HOLD BUT DO NOT FORGET
 // var _scale_fontSize_prcnt = nScale => R.compose(R.flip(R.concat)('%'), R.toString, R.multiply(nScale), parseFloat);
 
