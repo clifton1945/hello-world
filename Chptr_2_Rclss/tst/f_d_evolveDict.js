45ee6758ec13cc6188a0a06c6c4161ea162f5f5b
clif edwards <clif.edwards.2@gmail.com>
2016-07-15 08:08:32 -0400

    * f_evolve_dCSD.js  * 160715 @ 0800 -> STABLE test"0  ***** _revolveD trgt_CSDs_D USING a partialed_scale_opacity(.5, R.__) *****"  *      REFACT _scale_opacity TO function w/ arity 2 allowing  new scale weight factor AND  *      REBUILT evolve TO revolve: flipped to partial the nearly constant style property CSD dictionaries  * 160714  @0602 -> new day. LEARN to compose transforms for use in f_evolve_dCSD()

--- Chptr_2_Rclss/tst/f_d_evolveDict.js
+++ Chptr_2_Rclss/tst/f_d_evolveDict.js
@@ -1,9 +1,12 @@
 /**
  * f_evolve_dCSD.js
- * 160714  @0602 -> new day. LEARN to compose tranforms for use in f_evolve_dCSD()
+ * 160715 @ 0800 -> STABLE test"0  ***** _revolveD trgt_CSDs_D USING a partialed_scale_opacity(.5, R.__) *****"
+ *      REFACT _scale_opacity TO function w/ arity 2 allowing  new scale weight factor AND
+ *      REBUILT evolve TO revolve: flipped to partial the nearly constant style property CSD dictionaries
+ * 160714  @0602 -> new day. LEARN to compose transforms for use in f_evolve_dCSD()
  * 160713 -> @0900 WIP somehow apply scale wt to the evolve  transformers.
  *      STABLE because hardcoded.
- * 160712 -> @1415  WIP BROKEN how pass in the Mapper( key, function}s???
+ * 160712 -> @1415  WIP BROKE how pass in the Mapper( key, function}s???
  *  @1235 ADDED _scale_fontSize_prcnt && for the future scale_fontSize_px && test.2
  * 160707
  *  WIP STABLE and 1st test
@@ -11,56 +14,38 @@
 "use strict";
 let R = require('ramda');
 
-var stubScale = .5;
-var stubOffset = -.6;
-let f_offset_N = R.add;
-let f_scaleN_ = R.multiply; //N->N->N
+// var stubScale = .5;
+// var stubOffset = -.6;
+let f_offsetN = R.add;
+let f_scaleN = R.multiply; //N->N->N
 // ---------------------- Code Under Test: f_evolve_dCSD
-let _scaleN_ = f_scaleN_(stubScale);
-/**
- * _scale_opacity:: N:wt -> S:a -> S:wt x a
- * @param wt
- */
-var _scale_opacity = R.compose(R.toString, _scaleN_, parseFloat);
+// let _scaleN = f_scaleN(stubScale);
+var _scale_opacity = (wt, propVal) => R.compose(R.toString, f_scaleN(wt), parseFloat);// N:wt -> S:propVal -> S:
+
 var _scale_fontSize_prcnt = nScale => R.compose(R.flip(R.concat)('%'), R.toString, R.multiply(nScale), parseFloat);
 // below: scale_fontSize_px is used in getComputerStyles CSD's
 // var scale_fontSize_px = R.compose(R.flip(R.concat)('px'), R.toString, R.multiply(nScale), parseFloat);
 // WIP FIX
-var scale_fontSize = wt => _scale_fontSize(wt);
 /**
- *      _dMappers:: {k:{v->v}}
- * @type {{opacity: ((p1?:*)=>Function), fontSize: ((p1?:*)=>Function)}}
- * @private
+ *      f_revolve_trgtDict:: D:{k:v} -> F:{k:{v->v}} -> D:{k:v}
+ *
+ *      typical use:: newCSD = f_revolve_trgtDict( trgt_CSDs_D)(_trnsfrm_theseCSDs_D)
+ *          trgt_CSDs_D:  {opacity: 1, fontSize:'100%', ...} // typically a composite CSD for a verse:
+ *          _trnsfrm_theseCSDs_D: {opacity: trnsfrm_opacity, fontSize: trnsfrm_fontSize}
  */
-const f_dMappers =  {
-    opacity: _scale_opacity,
-};
-
-/**
- *      f_evolve_dCSD:: F:{k:{v->v}} -> D:{k:v} -> D:{k:v}
- *      D:{k:v}f_dMappers, D:dCSD, N:nScale -> D:dCSD
- *  scales CSD properties hardcoded into this D:: f_dMappers
- *  some configurations of this might include:
- *      f_dMappers as parameter,
- * @param dCSD ->
- * @return dCSD
- * @param _dMappers
- */
-const f_evolve_dCSD = function f_evolve_dCSD(_dMappers, dCSD) {
-    return R.evolve(_dMappers, dCSD); //=> (D -> D) -> N -> D
-};
-const _scale_dCSD = R.curry(f_evolve_dCSD);
-
+const f_revolve_trgtDict = R.flip(R.evolve);
 
 // exports
-module.exports = {_scale_dCSD};
-
+// module.exports = {????????????};
 
 // ---------------------- test: f_evolve_dCSD
 let test = require('tape');
-// FIX TESTS PASS because the f_mappers have hardcoded wt factors!
-test("0  ***** f_evolve_dCSD/ _scale_dCSD() *****", function (t) {
-    t.deepEquals(_scale_dCSD(f_dMappers)({opacity: "1"}), {opacity: "0.5"}, ' opacity:".5"');
+var trgt_CSDs_D =  {opacity: "1", fontSize:'100%'};
+var _revolveCSD = f_revolve_trgtDict(trgt_CSDs_D); // NOW just needs the _trnsfrm_theseCSDs_D()
+test("0  ***** _revolveD trgt_CSDs_D USING a partialed_scale_opacity(.5, R.__) *****", function (t) {
+    var _partialed_scale_opacity = _scale_opacity(0.5, R.__);//
+    var _trnsfrm_theseCSDs_D = {opacity: _partialed_scale_opacity}; // _scale_opacity is just one of many transforms
+    t.deepEquals(_revolveCSD(_trnsfrm_theseCSDs_D).opacity, "0.5", ' EXP: opacity:"1" -> "0.5"');
     // t.deepEquals(_scale_dCSD(f_dMappers)({fontSize: "100%"}), {fontSize: "50%"}, ' fontSize:"50%"');
     t.end();
 });
