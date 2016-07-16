fc465a545528e54b0b42c57b40ea657900cf4236
clif edwards <clif.edwards.2@gmail.com>
2016-07-16 16:59:41 -0400

    * f_d_update_CsdD.js -> update a Verse default Csd as a function of Space parameters.  * 160716 @ 1657 ->  see test #2 w/ QUESTION:  *           FIX -- do I need to run compose against the  current value of say this opacity?? Do not think so: just scale_x_1, offset_plus_0  and format should be enough  * @1046 -> ADDED offset TO  * STABLE:: t.deepEquals(trgt_CSDs_D.fontSize, "90%", ' EXP: fontSize:"100" -> "90%"');

--- Chptr_2_Rclss/tst/f_d_update_CsdD.js
+++ Chptr_2_Rclss/tst/f_d_update_CsdD.js
@@ -1,9 +1,14 @@
 /**
  * f_d_update_CsdD.js -> update a Verse default Csd as a function of Space parameters.
- * 160716  @0916 STABLE but trying to consolidate code to just accept a scale factor and an offset
+ * 160716 @ 1657 ->  see test #2 w/ QUESTION:
+ *           FIX -- do I need to run compose against the  current value of say this opacity?? Do not think so: just scale_x_1, offset_plus_0  and format should be enough
+ * @1046 -> ADDED offset TO
+ * STABLE:: t.deepEquals(trgt_CSDs_D.fontSize, "90%", ' EXP: fontSize:"100" -> "90%"');
+
+ *  @0916 STABLE but trying to consolidate code to just accept a scale factor and an offset
  * IN // update opacity
  * // COULD NOT ADD DEFAULT PARAMETERS -> f(a,b=1)(a*b)  BREAKS 160716
- * // COULD NOT R.compose ->_pre_scaled = (fctr) => R.compose(R.multiply, f_scaler(fctr));//BREAKS (N -> {*->N)
+ * // COULD NOT R.compose ->opacity_scaled = (fctr) => R.compose(R.multiply, f_scaler(fctr));//BREAKS (N -> {*->N)
  *  0715 -> begin to add _pre_offset to transformations.
  *  @0645 -> REFACT: bringing into comparison both R.over and R.evole methods of updating CSDs.
  *      WAS f_evolve_dCSD.js
@@ -41,55 +46,68 @@ let test = require('tape');
 var dflt_CSDs_D = {opacity: "1", fontSize: '100%'};
 var trgt_CSDs_D;
 var _trnsfrm_theseCSDs_D;
-var _pre_offset, _pre_scaled, _pre_scaled_CSD;
+var _pre_offset, opacity_scaled, opacity_scaled_CSD;
 test("0  ***** USING .evolve (and my revolve) to update trgt_CSDs_D *****", function (t) {
     let RET;
     let _scaler = v => R.identity(v);
-    _pre_scaled = R.multiply(_scaler(.3));// (N -> {*->N)
+    opacity_scaled = R.multiply(_scaler(.3));// (N -> {*->N)
     // update opacity
-    _pre_scaled_CSD = R.compose(R.toString, _pre_scaled, parseFloat);//N:wt -> S:propVal -> S:
+    opacity_scaled_CSD = R.compose(R.toString, opacity_scaled, parseFloat);//N:wt -> S:propVal -> S:
 
     // The method of Transformation
-    _trnsfrm_theseCSDs_D = {opacity: _pre_scaled_CSD}; // WORKS _pre_scaled_CSD is just one of many transforms
+    _trnsfrm_theseCSDs_D = {opacity: opacity_scaled_CSD}; // WORKS opacity_scaled_CSD is just one of many transforms
     var trgt_CSD_D = f_revolve_trgtDict(dflt_CSDs_D, _trnsfrm_theseCSDs_D);
 
     t.deepEquals(
         trgt_CSD_D.opacity, "0.3", ' EXP: opacity:"1" -> "0.3"');
     t.end();
 });
-test("1  ***** USING .lensProp && .over  to update trgt_CSDs_D ******", function (t) {
+test("1  ***** USING .lensProp&&.over to update trgt_CSDs_D ******", function (t) {
     var RET;
-    var _scaler =  s => R.identity(s);
-    var f_scaler = s => R.always(s);
-    var _offsetter = o => R.identity(o);
-
-    // update opacity
-    // COULD NOT ADD DEFAULT PARAMETERS -> f(a,b=1)(a*b)  BREAKS 160716
-    // COULD NOT R.compose ->_pre_scaled = (fctr) => R.compose(R.multiply, f_scaler(fctr));//BREAKS (N -> {*->N)
-    // _pre_scaled = (fctr) => R.multiply(_scaler(fctr), R.__);//OK (N -> {*->N)
-    _pre_scaled = (fctr) => R.multiply(_scaler(fctr));//OK (N -> {*->N)
-    _pre_offset = R.add(_offsetter('0'));
-
+    opacity_scaled = R.multiply;//OK (N -> {*->N)
     var opacityLens = R.lensProp("opacity");
-    _pre_scaled_CSD = R.compose(R.toString, _pre_scaled(.3), _pre_offset, parseFloat);//N:wt -> S:propVal -> S:
+    opacity_scaled_CSD = scale_fctr => R.compose(R.toString, opacity_scaled(scale_fctr), parseFloat);//N:wt -> S:propVal -> S:
+    trgt_CSDs_D = R.over(opacityLens, opacity_scaled_CSD("0.3"), dflt_CSDs_D); // CUT
 
-    // The method of Transformation
-    //noinspection UnnecessaryLocalVariableJS
-    trgt_CSDs_D = R.over(opacityLens, _pre_scaled_CSD, dflt_CSDs_D);
-
-    //noinspection UnnecessaryLocalVariableJS
-    t.deepEquals(trgt_CSDs_D.opacity, "0.3", ' EXP: opacity:"1" -> "0.3"');
+    t.deepEquals(trgt_CSDs_D.opacity, "0.3", ' EXP: opacity:"1" SCALED TO "0.3"');
 
     // update fontSize
-    _pre_scaled = R.multiply(_scaler(.3));// (N -> {*->N)
+    var fontSize_offset = R.add;
     var fontSizeLens = R.lensProp("fontSize");
-    _pre_scaled_CSD = R.compose(R.flip(R.concat)('%'), R.toString, _pre_scaled, parseFloat);
-
-    // The method of Transformation
-    //noinspection UnnecessaryLocalVariableJS
-    trgt_CSDs_D = R.over(fontSizeLens, _pre_scaled_CSD, dflt_CSDs_D);
+    var fontSize_offset_CSD = (offset_val) => R.compose(R.flip(R.concat)('%'), R.toString, fontSize_offset(offset_val), parseFloat);
+    var trgt_CSDs_D = R.over(fontSizeLens, fontSize_offset_CSD(-10), dflt_CSDs_D);// CUT
 
     //noinspection UnnecessaryLocalVariableJS
-    t.deepEquals(trgt_CSDs_D.fontSize, "30%", ' EXP: fontSize:"100" -> "30%"');
+    t.deepEquals(trgt_CSDs_D.fontSize, "90%", ' EXP: fontSize:"100" OFFSET TO "90%"');
+
+    t.end();
+});
+test("2  ***** USING BOTH opacity_scale() and opacity_offset().lensProp&&.over to update opacity_scale_CSDs_D ******", function (t) {
+    var RET;
+
+    var opacityLens = R.lensProp("opacity");
+    var opacity_scale = R.multiply;//OK (N -> {*->N)
+    var opacity_offset = R.add;// N ->
+    /**
+     *      opacity_trnsfrms_CSD:: (N:scale, N:offset_plus_0) -> D:CSD
+     * @param scale_x_1 * 1 | 100
+     * @param offset_plus_0 + 0
+     */
+    var opacity_trnsfrms_CSD = (scale_x_1, offset_plus_0 ) => R.compose(
+        R.toString, opacity_offset(offset_plus_0), opacity_scale(scale_x_1), parseFloat);//N:wt -> S:propVal -> S:
+    // FIX -- do I need to run compose against the  current value of say this opacity?? Do not think so: just scale_x_1, offset_plus_0  and format should be enough
+    var dflt_CSDs_D = {opacity: "1", fontSize: '100%'};
+    var trgt_CSDs_D = R.over(opacityLens, opacity_trnsfrms_CSD("1", "0"), dflt_CSDs_D); // CUT
+    t.deepEquals(trgt_CSDs_D.opacity, "1", ' EXP: opacity:"1" SCALED(0, 1) TO "1"');
+
+    var trgt_CSDs_D = R.over(opacityLens, opacity_trnsfrms_CSD("0", "0"), dflt_CSDs_D); // CUT
+    t.deepEquals(trgt_CSDs_D.opacity, "0", ' EXP: opacity:"0" SCALED(0, 0) TO "0"');
+
+    var trgt_CSDs_D = R.over(opacityLens, opacity_trnsfrms_CSD("1",".25"), dflt_CSDs_D); // CUT
+    t.deepEquals(trgt_CSDs_D.opacity, ".25", ' EXP: opacity:"1" SCALED(1,0.6) TO "1.6"');
+
+    var trgt_CSDs_D = R.over(opacityLens, opacity_trnsfrms_CSD("-0.2","0.25"), dflt_CSDs_D); // CUT
+    t.deepEquals(trgt_CSDs_D.opacity, "8.2", ' EXP: opacity:"1" SCALED(-.2, .8) TO "0.32"');
+
     t.end();
 });
