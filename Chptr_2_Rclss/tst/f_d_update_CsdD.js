4721ea575ec08be4f0262f56bec53fffb6dcda6f
clif edwards <clif.edwards.2@gmail.com>
2016-07-18 18:34:04 -0400

    * f_d_update_CsdD.js  * 160718  @1830 -> ADDED calcWt FROM f_n_calcWt-compiled.js AND  *      test("1  ***** USING calcWt WITH .lensProp&&.over to update trgt_CSDs_D ******" IS STABLE  *  *  @ 1203 ->  ready to just REPLACE the present csd.value WITH a calculated weight FOR a given csd.key.  *      in doing so I am skipping test#0 and #2 and deleting the fontSize test  * f_d_update_CsdD.js -> update a Verse default Csd as a function of Space parameters.

--- Chptr_2_Rclss/tst/f_d_update_CsdD.js
+++ Chptr_2_Rclss/tst/f_d_update_CsdD.js
@@ -1,6 +1,9 @@
 /**
- * f_d_update_CsdD.js -> update a Verse default Csd as a function of Space parameters.
- * 160718 @ 1203 ->  ready to just REPLACE the present csd.value WITH a calculated weight FOR a given csd.key.
+ * f_d_update_CsdD.js
+ * 160718  @1830 -> ADDED calcWt FROM f_n_calcWt-compiled.js AND
+ *      test("1  ***** USING calcWt WITH .lensProp&&.over to update trgt_CSDs_D ******" IS STABLE
+ *
+ *  @ 1203 ->  ready to just REPLACE the present csd.value WITH a calculated weight FOR a given csd.key.
  *      in doing so I am skipping test#0 and #2 and deleting the fontSize test
  *  0650 ->  what do I want? How about for now - to get off the dime
  *  (2)USE 4 parameters to set a verse style CSD : valBeg, valEnd, ndxMe, lenFam
@@ -10,22 +13,7 @@
  *          this is typically ndx/(len-1) ** SEE f_n_Wter fo details of Rclss difference
  *      WHICH has the following Problem: how set the endValue ??
  *          without the endValue
- * 160716 @ 1657 ->  see test #2 w/ QUESTION:
- *           FIX -- do I need to run compose against the  current value of say this opacity?? Do not think so: just scale_x_1, offset_plus_0  and format should be enough
- * @1046 -> ADDED offset TO
- * STABLE:: t.deepEquals(trgt_CSDs_D.fontSize, "90%", ' EXP: fontSize:"100" -> "90%"')
- *  @0916 STABLE but trying to consolidate code to just accept a scale factor and an offset
- * IN // update opacity
- * // COULD NOT ADD DEFAULT PARAMETERS -> f(a,b=1)(a*b)  BREAKS 160716
- * // COULD NOT R.compose ->opacity_scaled = (fctr) => R.compose(R.multiply, f_scaler(fctr));//BREAKS (N -> {*->N)
- *  0715 -> begin to add _pre_offset to transformations.
- *  @0645 -> REFACT: bringing into comparison both R.over and R.evole methods of updating CSDs.
- *      WAS f_evolve_dCSD.js
- *      WILL DO  (0) ADD offset and maybe mirror transforms (1) REFACT scale / weight as functions of (ndx and faml.length)
- * then map over all verses in Chptr Space
- * then add Rclss Space transforms
- * 160715  @1625 -> test("1  ***** USING .lensProp && .over update_a trgt_CSDs_D ***** STABLE
- *
+ * f_d_update_CsdD.js -> update a Verse default Csd as a function of Space parameters.
  */
 "use strict";
 let R = require('ramda');
@@ -49,14 +37,14 @@ let test = require('tape');
 var dflt_CSDs_D = {opacity: "1", fontSize: '100%'};
 var trgt_CSDs_D;
 var _trnsfrm_theseCSDs_D;
-var _pre_offset, opacity_scaled, opacity_scaled_CSD;
+var _pre_offset, opacity_weighter, opacity_weighter_CSD;
 test("0  ***** USING .evolve (and my revolve) to update trgt_CSDs_D *****", {skip:true},
     function (t) {
     let RET;
     let _scaler = v => R.identity(v);
-    opacity_scaled = R.multiply(_scaler(.3));// (N -> {*->N)
+    var opacity_scaled = R.multiply(_scaler(.3));// (N -> {*->N)
     // update opacity
-    opacity_scaled_CSD = R.compose(R.toString, opacity_scaled, parseFloat);//N:wt -> S:propVal -> S:
+    var opacity_scaled_CSD = R.compose(R.toString, opacity_scaled, parseFloat);//N:wt -> S:propVal -> S:
 
     // The method of Transformation
     _trnsfrm_theseCSDs_D = {opacity: opacity_scaled_CSD}; // WORKS opacity_scaled_CSD is just one of many transforms
@@ -66,17 +54,32 @@ test("0  ***** USING .evolve (and my revolve) to update trgt_CSDs_D *****", {ski
         trgt_CSD_D.opacity, "0.3", ' EXP: opacity:"1" -> "0.3"');
     t.end();
 });
-test("1  ***** USING .lensProp&&.over to update trgt_CSDs_D ******", function (t) {
+test("1  ***** USING calcWt WITH .lensProp&&.over to update trgt_CSDs_D ******", function (t) {
     var RET;
-    var opacityLens = R.lensProp("opacity");
-    // CUT what if  opacity_scaled just REPLACED the key:value ??
-    opacity_scaled =  wt => R.always(wt);
-    // opacity_scaled = R.multiply;//OK (N -> {*->N)
-    opacity_scaled_CSD = scale_fctr => R.compose(R.toString, opacity_scaled(0.3), parseFloat);//N:wt -> S:propVal -> S:
-    trgt_CSDs_D = R.over(opacityLens, opacity_scaled_CSD("0.3"), dflt_CSDs_D); // CUT
-    // trgt_CSDs_D = R.over(opacityLens, opacity_scaled_CSD("0.3"), dflt_CSDs_D); // CUT
+    /**
+     *      calcWt():: ( D:spanCsd -> L:famlElem) -> N:ndxElem -> N: wter
+     *      USED: typically to weight element property CSD: e.g. opacity, fontSize, etc
+     *          the L:fam and N:ndx will be returned by indexedMaps typically
+     *
+     * @param sObj  -> style Property beg and end limits
+     * @param l_fam -> list of this elements family
+     * @param n_ndx -> the index of this elem in the family list
+     * @returns {*} -> an Element weight for this context.
+     */
+    // var calcWt = require('./f_n_calcWt'); //
+    var calcWt = require('./f_n_calcWt-compiled'); //
+    var _calcWt = R.curry(calcWt); //
 
-    t.deepEquals(trgt_CSDs_D.opacity, "0.3", ' EXP: opacity:"1" SCALED TO "0.3"');
+    const rng = {smlWt:0.5, lrgWt:0.9};
+    const ary = [0,1,2,3,4,5,6];
+    var ndx = 6;
+    opacity_weighter =  R.always(_calcWt(rng, ary)(ndx)); // NOTE: apply ndx BEFORE the compose!!!!
+    opacity_weighter_CSD = R.compose(R.toString, opacity_weighter, parseFloat);//N:wt -> S:propVal -> S:
+
+    var opacityLens = R.lensProp("opacity");
+    trgt_CSDs_D = R.over(opacityLens, opacity_weighter_CSD, dflt_CSDs_D); // CUT
+    // trgt_CSDs_D = R.over(opacityLens, opacity_weighter_CSD("0.3"), dflt_CSDs_D); // CUT
+    t.deepEquals(trgt_CSDs_D.opacity, "0.5", ' EXP: opacity:"1" SCALED TO "0.5"');
     t.end();
 });
 test("2  ***** USING BOTH opacity_scale() and opacity_offset().lensProp&&.over to update opacity_scale_CSDs_D ******", {skip:true},
