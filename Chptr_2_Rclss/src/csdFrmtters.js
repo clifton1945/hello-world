0f37e3ecda05bd247e84aabef61ad45b1396d4d5
clif edwards <clif.edwards.2@gmail.com>
2016-07-28 06:24:25 -0400

    * csdFrmttrs::  * 160728   @0617 -> refact parameter Name: TO wt FROM ndx.  *      Reflecting the next REFACT in update_CsdD.js: REMOVING dependence on passing _calcWt; just use a Wt,

--- Chptr_2_Rclss/src/csdFrmtters.js
+++ Chptr_2_Rclss/src/csdFrmtters.js
@@ -1,9 +1,11 @@
 /**
  * csdFrmttrs::
+ * 160728   @0617 -> refact parameter Name: TO wt FROM ndx.
+ *      Reflecting the next REFACT in update_CsdD.js: REMOVING dependence on passing _calcWt; just use a Wt,
  * 160722   @1805 -> REMOVED _calcWt. This exports just the Formatters; NAME CHANGED from  csdWters.js
  * @1600 -> weighter that need an updated _calcWt()
- * _opacityFrmttr::     N:ndx -> S:wt
- * _fontSizeFrmttr::    N:ndx -> S:wt
+ * _opacityFrmttr::     N:wt -> S:wt
+ * _fontSizeFrmttr::    N:wt -> S:wt
  *
  * IN FILE: csdFrmttrs.js
  */
@@ -14,15 +16,15 @@ var myTap = require('./h').myTap;
 var my_toFixed = require('./h').my_toFixed;
 
 /**
- *      _opacityFrmttr:: N:ndx -> S:wt
- *      formats opacity _calcWt return
- * @param f_calcWt
+ *      _opacityFrmttr:: N:wt -> S:wt
+ *      formats opacity N:wt. Typically from a previous _calcWt call.
+ // * @param f_calcWt
  * @private
  */
-var _opacityFrmttr =  my_toFixed(3);// N:ndx -> S:wt
+var _opacityFrmttr =  my_toFixed(3);// N:wt -> S:wt
 /**
- *      _fontSizeFrmttr:: N:ndx -> S:wt
- *      formats fontSize _calcWt return
+ *      _fontSizeFrmttr:: N:wt -> S:wt
+ *       formats opacity N:wt. Typically from a previous _calcWt call.
  * @param f_calcWt
  * @private
  */
@@ -32,7 +34,7 @@ var _fontSizeFrmttr = R.compose(
     // myTap,
     my_toFixed(0),
     R.multiply(100)
-); // N:ndx -> S:wt
+); // N:wt -> S:wt
 
 assert.equal(_opacityFrmttr(0.6334), "0.633", '_opacityFrmttr(0.6334) -> "0.633"');
 assert.equal(_fontSizeFrmttr(0.6334), "63%", '_fontSizeFrmttr(0.6334) -> "63%"');
