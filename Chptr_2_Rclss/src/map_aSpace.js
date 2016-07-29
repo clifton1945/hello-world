f1ecc6534d02bcc1a05a27a1dc30a8e06e361e49
clif edwards <clif.edwards.2@gmail.com>
2016-07-29 08:18:57 -0400

    *  map_aSpace:: D -> L:elems -> L:elems  *  160729  @0820   -> FIX This  *  160726  @0355   ->  *  -> SET each Verse CSD as a function of their Space parameters  *  map_aSpace.js   -> SET each Verse CSD as a function of their Space parameters  *  IN FILE: map_aSpace.js

--- Chptr_2_Rclss/src/map_aSpace.js
+++ Chptr_2_Rclss/src/map_aSpace.js
@@ -1,5 +1,6 @@
 /**
  *  map_aSpace:: D -> L:elems -> L:elems
+ *  160729  @0820   -> FIX This
  *  160726  @0355   ->
  *  -> SET each Verse CSD as a function of their Space parameters
  *  map_aSpace.js   -> SET each Verse CSD as a function of their Space parameters
@@ -10,10 +11,10 @@
 var R = require('ramda');
 var assert = require('assert');
 var C_Both = require('./h').C_Both;
-// var _n_calcWt = require('./calcWt')._n_calcWt;
-var _set_trgt_Csds = require('./update_CsdD')._set_trgt_Csds;//Fn:(* -> N:wt) -> N:ndx -> D:inCsd -> D:outCSD
+var _n_calcWt = require('./calcWt')._n_calcWt;//        _n_calcWt:: D:lmntD -> L:fam -> Fn:(N:elemNdx -> N:wt)
+var _set_trgt_Csds = require('./update_CsdD')._trgt_Csds;// _trgt_Csds:: (N:wt) -> D:inCsd -> D:outCSD
 //GLOBALS
-console.log(JSON.stringify("In map_aSpace.js."));
+console.log(JSON.stringify("IN map_aSpace.js."));
 
 // MAIN CodeUnderTest
 var f_wter =  R.curry(( f_calcWt, e, n, l) => R.compose(
@@ -30,9 +31,14 @@ var _wter = R.curry(
     Object.assign(e.style, trgt_Csds);
     return n;
 });// Fn:(*-> N) -> E:
+
 // var myMap = R.addIndex(R.map);
 // myMap(_wter);
 // let fnX = (fn, csdD) =>  R.addIndex(R.map)(f_wter(fn, R.__));// E:html -> N:ndx
-let _map_aSpace = (fn) =>  R.addIndex(R.map)(_wter(fn));// E:html -> N:ndx
-module.exports =  { _map_aSpace};
+let _map_aSpace = (fn) =>  R.addIndex(R.map)(_wter(fn));// _map_aSpace:: ?????????????
+
+console.log(JSON.stringify("OUT map_aSpace.js."));
+
+// _map_aSpace:: ?????????????
+module.exports =  {_map_aSpace};
 //
