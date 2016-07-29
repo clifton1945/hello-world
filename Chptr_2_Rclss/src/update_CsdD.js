27018e6015d3362194603d785b3924e39b99947d
clif edwards <clif.edwards.2@gmail.com>
2016-07-29 12:58:38 -0400

    * update_CsdD.js  * 160729   @1300   -> STABLE Tests. How add D:inCsd in after composed the two formats??  *  @0815 -> REFACT  comment

--- Chptr_2_Rclss/src/update_CsdD.js
+++ Chptr_2_Rclss/src/update_CsdD.js
@@ -1,6 +1,6 @@
 /**
  * update_CsdD.js
- * 160729   @1110 -> STABLE Tests. How add D:inCsd in after composed the two formats??
+ * 160729   @1300   -> STABLE Tests. How add D:inCsd in after composed the two formats??
  *  @0815 -> REFACT  comment
  * 160728   @1910 -> STABLE tests
  *      @1735   -> BEGIN to use fn from csdValueS.js
@@ -30,23 +30,28 @@ var cut;
         cut = _set_csdD_fontSize(.5);
         assert.equal(cut({}).fontSize, "50%", 'fontSize:: wt:.5 SET TO "50%" USING _set_csdD_fontSize()');
 var noop = 0;
-// // NOW CHAIN THE TWO:  _set_trgt_fontSize, _set_set_trgt_opacityCsdD
 
+// NOW CHAIN THE TWO:  _set_trgt_fontSize, _set_set_trgt_opacityCsdD
+// TO result in _trgt_Csds:: N:wt -> D:inCsd -> D:outCsd
+//  BUT NOW I need
+var trgt_Csds;
 // const _trgt_Csds =  function _trgt_Csds(in_csd, wtN) {
 const _trgt_Csds =  function _trgt_Csds(wtN) {
-    // var x = _set_csdD_opacity(wtN, R.__); // x:: N -> D -> D
-    // var y = _set_csdD_fontSize(wtN, R.__);
-    // var z =  (in_csd) => R.compose(x, y);// D:inCsd  -> outCsd
-    // return z;// (D:in_csd) -> D:out_csd
     return R.compose(_set_csdD_opacity(wtN, R.__), _set_csdD_fontSize(wtN, R.__));// N:wt -> D:inCsd  -> outCsd
 };//  _trgt_Csds:: N:wt -> D:inCsd -> D:outCsd
     //asserts
-    //     var trgt_Csds = _trgt_Csds({backgroundColor: 'lightGreen'})(0.5);
-        var trgt_Csds = _trgt_Csds(0.5)({backgroundColor: 'lightGreen'});
+        trgt_Csds = _trgt_Csds(0.5)({backgroundColor: 'lightGreen'});
         assert.equal(trgt_Csds.fontSize, "50%", 'fontSize:: wt:.5 SET TO "50%" USING cut = _trgt_Csds({})');
         assert.equal(trgt_Csds.opacity, "0.500", 'opacity:: wt:.5 SET TO "0.500" USING cut = _trgt_Csds({})');
         assert.equal(trgt_Csds.backgroundColor, 'lightGreen', "_trgt_Csds.backgroundColor -> 'lightGreen' ");
 
+const _csds = R.curry((csd, wt) => _trgt_Csds(wt)(csd));// D -> N -> D
+    //asserts
+        var Csds = _csds({backgroundColor: 'lightGreen'}, 0.5);
+        assert.equal(Csds.fontSize, "50%", 'fontSize:: wt:.5 SET TO "50%" USING cut = _Csds({})');
+        assert.equal(Csds.opacity, "0.500", 'opacity:: wt:.5 SET TO "0.500" USING cut = _Csds({})');
+        assert.equal(Csds.backgroundColor, 'lightGreen', "_Csds.backgroundColor -> 'lightGreen' ");
+
 //   * -----------------------  EXPORTS --------------------
 //  */
-module.exports = {_trgt_Csds};// _trgt_Csds:: N:wt -> D:inCsd  -> outCsd
+module.exports = {_csds, _trgt_Csds};// _trgt_Csds:: N:wt -> D:inCsd  -> outCsd
