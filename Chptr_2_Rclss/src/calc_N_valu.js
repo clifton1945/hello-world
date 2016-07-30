f70cfa270a9b017846a64d2f906df41f24a41ec4
clif edwards <clif.edwards.2@gmail.com>
2016-07-30 10:45:05 -0400

    * _calc_N_valu  * 160730   @1000 -> RENAMED _calc_N_valu([D:csdSpan, L_fam]) -> N_elemNdx  --> N_valu  *          will DEPRECATE _n_calc()  * 160729   @0745 -> REFACT var names  * _n_calcWt:: (D->L) -> N:elemNdx -> N:wt  * 160722  @0605 -> BUILT a usable fn: _n_calcWt(D, N) which returns a fn(N:elemNdx) -> N:wt  * IN FILE: _calc_N_valu.js -> CALCULATE a weighting factor AS a function of a Verse Context/Space parameters: csd D_csdSpan, familyL, itsNdxN.

---
+++ Chptr_2_Rclss/src/calc_N_valu.js
@@ -0,0 +1,96 @@
+/**
+ * _calc_N_valu
+ * 160730   @1000 -> RENAMED _calc_N_valu([D:csdSpan, L_fam]) -> N_elemNdx  --> N_valu
+ *          will DEPRECATE _n_calc()
+ * 160729   @0745 -> REFACT var names
+ * _n_calcWt:: (D->L) -> N:elemNdx -> N:wt
+ * 160722  @0605 -> BUILT a usable fn: _n_calcWt(D, N) which returns a fn(N:elemNdx) -> N:wt
+ * IN FILE: _calc_N_valu.js -> CALCULATE a weighting factor AS a function of a Verse Context/Space parameters: csd D_csdSpan, familyL, itsNdxN.
+ */
+"use strict";
+let R = require('ramda');
+
+    /**
+     *      f_calc_N_valu(D_csdSpan, L_fam, N_ndx):: -> N_valu
+     *      USED: typically used to weight element property CSD: e.g. opacity, fontSize, etc
+     *      typically the L:fam and N:elemNdx will be returned by indexedMaps.
+     * @param D_csdSpan   -> {smlWt:a, lrgWt:a}   style Property beg and end limits
+     * @param L_fam  -> [a] list of this elements family
+     * @param N_ndx  -> a the index of this elem in the family lis
+     * @returns {*} -> an Element weight for this context.
+     * @private
+     */
+let f_calc_N_valu = R.curry(function _calc_Nvalu(D_csdSpan, L_fam, N_ndx) {
+        var smlWt = D_csdSpan.smlWt;
+        var lrgWt = D_csdSpan.lrgWt;
+        var len = L_fam.length - 1;
+        return len > 0 ? -(lrgWt - smlWt) / len * N_ndx + lrgWt : lrgWt; // always lrgWt
+    }); // D->L -> N -> N:wt
+    /**
+ *  _calc_N_valu([D_csdSpan, L_fam]):: N_elemNdx -> N_valu
+ * @param D_csdSpan
+ * @param L_fam
+ * @private
+ */
+const _calc_N_valu = R.partial(f_calc_N_valu);//:: L:[D_csdSpan, L_fam] -> N_elemNdx -> N_valu
+
+// CODE UNDER TEST: _calc_N_valu:: N_ndx -> N_valu
+var stub = [0, 1, 2, 3, 4, 5, 6];
+var fn = _calc_N_valu([{smlWt:0.5, lrgWt:0.9}, stub]);
+var assert = require("assert");
+assert.equal(fn( 0), 0.9, 'exp: 0.9 FROM assert _calc_N_valu(0)');
+assert.equal(fn(6), 0.5, 'exp: 0.5 FROM assert _calc_N_valu(6)');
+assert.equal(fn(4), 0.6333333333333333, 'exp: 0.633... FROM assert _calc_N_valu(4)');
+
+
+//************************** probably DEPRECATE 160730 ********************
+/**
+ *      f_n_calcWt():: ( D:spanCsd -> L:famElem) -> N:elemNdx -> N: wter
+ *      USED: typically to weight element property CSD: e.g. opacity, fontSize, etc
+ *          the L:fam and N:elemNdx will be returned by indexedMaps typically
+ *
+ * @param sObj  -> {smlWt:a, lrgWt:a}   style Property beg and end limits
+ * @param l_fam -> [a] list of this elements family
+ * @param n_ndx -> a the index of this elem in the family list
+ * @returns {*} -> an Element weight for this context.
+ */
+var f_n_calcWt = R.curry(function f_n_calcWt(spanD, famL, ndxN) {
+    var smlWt = spanD.smlWt;
+    var lrgWt = spanD.lrgWt;
+    var len = famL.length - 1;
+    return len > 0 ? -(lrgWt - smlWt) / len * ndxN + lrgWt : lrgWt; // always lrgWt
+}); // D->L->N -> N:wt
+
+/**
+*      _n_calcWt:: D:lmntD -> L:fam -> Fn:(N:elemNdx -> N:wt)
+*      Partials _calc_Nvalu
+ */
+const _n_calcWt = R.curry(function _n_calcWt(lmntD, L_fam) {
+    return f_n_calcWt(lmntD, L_fam)
+});// N:elemNdx -> N:wt
+
+// CODE UNDER TEST: _n_calcWt()//N:elemNdx -> N:wt
+// var assert = require("assert");
+
+fn = _n_calcWt({smlWt:0.5, lrgWt:0.9})([0, 1, 2, 3, 4, 5, 6]);
+assert.equal(fn(0), 0.9, 'FAILED assert _calcWt(0)');
+assert.equal(fn(6), 0.5, 'FAILED assert _calcWt(6)');
+assert.equal(fn(4), 0.6333333333333333, 'FAILED assert _calcWt(4');
+
+// MODULES.EXPORT
+// _calc_Nvalu():: ( D:spanCsd -> L:famElem) -> N:elemNdx -> N: wter
+// _n_calcWt:: D:lmntD -> L:fam -> Fn:(N:elemNdx -> N:wt)
+module.exports =  {_calc_N_valu, _n_calcWt};// where _n_calcWt will be deprecated.
+
+
+
+/**
+*      calcWt():: ( D:spanCsd -> L:famlElem) -> N:elemNdx -> N: wter
+*      USED: typically to weight element property CSD: e.g. opacity, fontSize, etc
+*          the L:fam and N:elemNdx will be returned by indexedMaps typically
+*
+* @param sObj  -> {smlWt:a, lrgWt:a}   style Property beg and end limits
+* @param l_fam -> [a] list of this elements family
+* @param n_ndx -> a the index of this elem in the family list
+* @returns {*} -> an Element weight for this context.
+*/
