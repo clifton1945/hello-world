391e2df914d28c82d8be14991568160cbaefdf25
clif edwards <clif.edwards.2@gmail.com>
2016-07-30 13:48:23 -0400

    * csdValueS.js  * 160730   @1350 STABLE revamped as composition given N_valu from _calc_N_valu()  *      @1330  WIP. revamp as composition given N_valu from _calc_N_valu()

--- Chptr_2_Rclss/src/csdValueS.js
+++ Chptr_2_Rclss/src/csdValueS.js
@@ -1,6 +1,7 @@
 /**
  * csdValueS.js
- * 160730   @1330  WIP. revamp as composition given N_valu from _calc_N_valu()
+ * 160730   @1350 STABLE revamped as composition given N_valu from _calc_N_valu()
+ *      @1330  WIP. revamp as composition given N_valu from _calc_N_valu()
  *  the process pipe: for a CSD_S_name
  *      N_valu -> _frmt_CSD_op(S_name_frmttr) -> S_valu -> _set_CSD_valu(S_nameLens, {}) -> CSD_valu
  * 160723   @1715 -> STABLE  module.exports = { _a_csdValu_opacity, _a_csdValu_fontSize}
@@ -12,8 +13,8 @@
 let R = require('ramda');
 let assert = require('assert');
 // BASE && HELPER FUNCTIONS
-let _opacityFrmttr = require('./csdFrmtters')._opacityFrmttr;//        N:wt -> S:wt
-let _fontSizeFrmttr = require('./csdFrmtters')._fontSizeFrmttr; //     N:wt -> S:wt
+let _opacityFrmttr = require('./csdFrmtters')._opacityFrmttr;//        N_valu -> S_valu
+let _fontSizeFrmttr = require('./csdFrmtters')._fontSizeFrmttr; //     N_valu -> S_valu
 let _opacityLens = R.lensProp('opacity');//         -> Lens
 let _fontSizeLens = R.lensProp('fontSize');//       -> Lens
 //  TEST DATA
@@ -21,39 +22,17 @@ var stub_N_ndx = 0;
 var stub_L_fam = [0, 1, 2, 3, 4, 5, 6];
 
 // BASE && HELPER FUNCTIONS
-/**
- *          _a_frmttd_value:: (Fn:propFrmttr, N:wt) || Fn:propFrmttr -> N:wt -> S:wt
- *
- * @param propFrmttr
- * @param wtN
- * @returns wtS
- */
-const f_a_frmttd_value = function f_a_frmttd_value(propFrmttr, wtN) { return propFrmttr(wtN)};// (Fn, N) -> S
-const _a_frmttd_value = R.curry(f_a_frmttd_value); //:: Fn:propFrmttr -> N:wt -> S:wt
-        // confirm _a_frmttd_value WORKS
-
-/**
- *      _a_csdValu:: ((S, Fn) || S -> Fn) -> N -> D:inCsd  ->  D:outCsd
- *      USE: sets S:property lensName
- * @param lensNameS
- * @param f_frmt
- * @param wtN
- */
-const f_a_csdValu = function f_a_csdValu (lensNameS, f_frmt, wtN) {
-    return R.set(R.lensProp(lensNameS), _a_frmttd_value(f_frmt)(wtN))};// (S, F, N) -> F:(D -> D:csd)
-const _a_csdValu = R.curry(f_a_csdValu);//      S -> Fn -> N  -> F:(D -> D:csd)
-
-
-
+const set_CSD_opacityValu = R.set(_opacityLens, R.__, {});// S_valu -> CSD_valu
+const set_CSD_fontSizeValu = R.set(_fontSizeLens, R.__, {});// S_valu -> CSD_valu
 
 /**
  *  NOW PRE FORMED opacity and fontSize
  */
-const _a_csdValu_opacity = _a_csdValu('opacity', _opacityFrmttr);//         N:wt -> D:inD -> D:outD
-const _a_csdValu_fontSize = _a_csdValu('fontSize', _fontSizeFrmttr);//      N:wt -> D:inD -> D:outD
-    // asserts
-    assert.equal(_a_csdValu_opacity(.6)({}).opacity,"0.600", '_a_csdValu_opacity');
-    assert.equal(_a_csdValu_fontSize(.6)({}).fontSize,"60%", '_a_csdValu_fontSize');
+const _a_csdValu_opacity = R.compose(set_CSD_opacityValu, _opacityFrmttr);
+const _a_csdValu_fontSize = R.compose(set_CSD_fontSizeValu, _fontSizeFrmttr);//      N:wt -> D:inD -> D:outD
+    //asserts
+    assert.equal(_a_csdValu_opacity(.6).opacity,"0.600", '_a_csdValu_opacity');
+    assert.equal(_a_csdValu_fontSize(.6).fontSize,"60%", '_a_csdValu_fontSize');
 
 
 /**
