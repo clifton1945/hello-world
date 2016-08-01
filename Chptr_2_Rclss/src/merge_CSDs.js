4a840aa77c88066a9137174a3be47d5b59baac64
clif edwards <clif.edwards.2@gmail.com>
2016-08-01 08:09:46 -0400

    HOORAH   *  main.js  AND CLEANED UP VERSIONS OF ITS Helper files.          *  16080   @0810 -> SET each Verse CSD as a function of its Space parameters IS STABLE TESTS and lightly tested          *      @0640   -> requiring just set_ElemStyle::Fn( E->N->L) - E          *  IN FILE: main.js -> SET each Verse CSD as a function of its Space parameters.

---
+++ Chptr_2_Rclss/src/merge_CSDs.js
@@ -0,0 +1,39 @@
+/**
+ *  merge_CSDs::  CSD -> N -> CSD
+ *  160730      @1800   -> STABLE & test
+ *  module.exports = {merge_CSDs};// merge_CSDs:: CSD_in  -> N_valu ->  CSD_final
+ *  IN FILE: merge_CSDs.js
+ */
+"use strict";
+
+let R = require('ramda');
+// HELPERS
+//  N_valu -> CSD_valu _opacity || _fontSize
+let _CSD_valu_opacity = R.curry(require('./set_CSD_valu')._CSD_valu_opacity);//         N:wt -> CSD_valu
+let _CSD_valu_fontSize = R.curry(require('./set_CSD_valu')._CSD_valu_fontSize); //      N:wt -> CSD_valu
+
+//   * -----------------------  EXPORTS --------------------
+/**
+ *      merge_CSDs:: CSD_init -> N_valu) -> CSD_final
+ *      use: COMBINES a CSD init and the two weighted CSD INTO one CSD
+ *      @return {{merge_CSDs: *}}
+ * @param CSD_init
+ * @param N_valu
+ */
+const merge_CSDs = R.curry(function merge_CSDs(CSD_init, N_valu) {
+    return R.mergeAll(
+        [CSD_init,
+            _CSD_valu_opacity(N_valu),
+            _CSD_valu_fontSize(N_valu)
+        ])
+});
+module.exports = {merge_CSDs};// merge_CSDs:: CSD_in  -> N_valu ->  CSD_final
+
+//asserts
+let assert = require('assert');
+var cut = merge_CSDs({backgroundColor: 'lightGreen'}, 0.5);
+assert.equal(cut.fontSize, "50%", 'fontSize:: wt:.5 SET TO "50%" USING cut = _Csds({})');
+assert.equal(cut.opacity, "0.500", 'opacity:: wt:.5 SET TO "0.500" USING cut = _Csds({})');
+assert.equal(cut.backgroundColor, 'lightGreen', "_Csds.backgroundColor -> 'lightGreen' ");
+
+
--- Chptr_2_Rclss/src/set_CSD_valu.js
+++ Chptr_2_Rclss/src/set_CSD_valu.js
@@ -1,9 +1,9 @@
 /**
- * _CSD_values_set:: N_valu -> CSD_valu Two values a_CSD_valu_opacity() AND _CSD_valu_fontSize()
+ * set_CSD_valu:: N_valu -> CSD_valu Two values a_CSD_valu_opacity() AND _CSD_valu_fontSize()
  * 160730   @1530   -> STABLE &&  RENAMED brought CSD fRMTTERS.JS into THIS
  *      @1350 STABLE revamped as composition given N_valu from _calc_N_valu()
  *      @1330  WIP. revamp as composition given N_valu from _calc_N_valu()
- * IN THE END -> _CSD_values_set.js fn(N:wt)  ->  CSD_valu
+ * IN THE END -> set_CSD_valu.js fn(N_valu)  ->  CSD_valu
  */
 "use strict";
 
@@ -14,35 +14,39 @@ var assert = require('assert');
 var my_toFixed = require('./h').my_toFixed;
 
 /**
- *      _opacityFrmttr:: N:wt -> S:wt
- *      formats opacity N:wt. Typically from a previous _calcWt call.
+ *      _opacityFrmttr:: N_valu -> S_valu
+ *      formats opacity N_valu. Typically from a previous _calcWt call.
  // * @param f_calcWt
  * @private
  */
-const _opacityFrmttr = my_toFixed(3);// N:wt -> S:wt
+const _opacityFrmttr = my_toFixed(3);// N_valu -> S_valu
 /**
- *      _fontSizeFrmttr:: N:wt -> S:wt
- *       formats opacity N:wt. Typically from a previous _calcWt call.
+ *      _fontSizeFrmttr:: N_valu -> S_valu
+ *       formats opacity N_valu. Typically from a previous _calcWt call.
  * @param f_calcWt
  * @private
  */
-const _fontSizeFrmttr = R.compose(R.flip(R.concat)('%'), my_toFixed(0), R.multiply(100)); // N:wt -> S:wt
+const _fontSizeFrmttr = R.compose(R.flip(R.concat)('%'), my_toFixed(0), R.multiply(100)); // N_valu -> S_valu
 
 let _opacityLens = R.lensProp('opacity');//         -> Lens
 let _fontSizeLens = R.lensProp('fontSize');//       -> Lens
 //  TEST DATA
 
 // BASE && HELPER FUNCTIONS
+/**
+ *       set_CSD_opacityValu => (_opacityLens, R.__, {})::  (S_valu) -> CSD_valu
+ */
 const set_CSD_opacityValu = R.set(_opacityLens, R.__, {});// S_valu -> CSD_valu
 const set_CSD_fontSizeValu = R.set(_fontSizeLens, R.__, {});// S_valu -> CSD_valu
 // functions
-const _CSD_valu_opacity = R.compose(set_CSD_opacityValu, _opacityFrmttr);//    N:wt -> CSD_valu_opacity
-const _CSD_valu_fontSize = R.compose(set_CSD_fontSizeValu, _fontSizeFrmttr);//    N:wt -> CSD_valu_fontSize
 
 /**
  * -----------------------  EXPORTS --------------------
  */
-module.exports = {_CSD_valu_opacity, _CSD_valu_fontSize};
+const _CSD_valu_opacity = R.compose(set_CSD_opacityValu, _opacityFrmttr);//    N_valu -> CSD_valu_opacity
+const _CSD_valu_fontSize = R.compose(set_CSD_fontSizeValu, _fontSizeFrmttr);//    N_valu -> CSD_valu_fontSize
+
+module.exports = {_CSD_valu_opacity, _CSD_valu_fontSize};//  N_valu -> CSD_valu_opacity || _fontSize
 
 //asserts
 var stub_N_ndx = 0;
