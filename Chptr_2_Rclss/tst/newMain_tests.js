54d22972b4e49fcca56bac90a72ceab9d4014be7
clif edwards <clif.edwards.2@gmail.com>
2016-08-11 09:39:20 -0400

    *  newMain.js  *  160811  0955  begin final compose to -> RClss_outerHTML  *  IN FILE: newMain_tests.js

--- Chptr_2_Rclss/tst/newMain_tests.js
+++ Chptr_2_Rclss/tst/newMain_tests.js
@@ -1,6 +1,6 @@
 /**
- *  newMain.js
- *  160811  0955  begin final compose to -> RClss_outerHTML
+ *  newMain_tests.js
+ *  160811  0940  many little tests heading towards one composed:
  *  IN FILE: newMain_tests.js
  */
 
@@ -9,12 +9,11 @@
 var R = require('ramda');
 var t = require('assert');
 
-var C_Both = require('./src/h').C_Both;
+var C_Both = require('../src/h').C_Both;
 
 /**
  *          ----- main helper functions -----
  */
-
 // var update_L_RClss_NLs = require('./src/update_L_RClss_NLs');//
 // var set_ElemStyle = require('./src/set_anElem_CSD');//
 //GLOBALS
@@ -32,12 +31,28 @@ const _S_outerHTML=el=>el.outerHTML;//:: E_a -> S_a
 var DIV_tst = document.querySelector('.Chptr_31');//:: div.Chptr_31
 var HTMLCol_Spans = DIV_tst.children;//-> HTMLCollection
 
+/**
+ *  ---     --- TESTS ---
+ */
 
+// // CAN return one Chptr SPAN as an outerHTML String:: El -> S
+// RET = _S_outerHTML(HTMLCol_Spans[2]);//D -> S
+// TST = R.slice(9,17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+//
 
 /**
  *  --- _L_S_outerHTMLs:: L_Els -> L_El_outerHTMLs
  */
 const _L_S_outerHTMLs = R.map(_S_outerHTML);// L_Els -> L_El_outerHTMLs
+
+// // CAN return a List/Array of Chptr SPANs as outerHTML Strings:: L_Els -> L_El_outerHTMLs
+// RET = _L_S_outerHTMLs(HTMLCol_Spans)[2];// L_Elems -> L_El_outerHTMLs[ndx] -> S_HTML_span
+// TST = R.slice(9,17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+
 /**
  *  --- _mySlice(d_lmits) -> L -> L_subset
  * @param d_lmits
@@ -47,6 +62,7 @@ const _mySlice = d_lmits => {
     var {beg, nxt} = d_lmits;// unpack
     return R.slice(beg, nxt)
 };//:: D_lmits -> L -> L_subset
+
 /**
  *  --- _RClss_S_outerHTML::D_Lmits -> L_S_spans -> L_S_spans
  * @param d_rcLmits
@@ -54,23 +70,31 @@ const _mySlice = d_lmits => {
  */
 const _RClss_S_outerHTML = d_rcLmits => _mySlice(d_rcLmits);// D_Lmits -> L_S_spans -> L_S_spans
 
-// WIP  NOW compose the whole thing:: HTMLCollections -> fn(D_lmits) -> S_RClss_innerHTML
-var fn = d_lmits => R.compose(
-    _RClss_S_outerHTML(d_lmits), _L_S_outerHTMLs
-);// D_lmits -> L_ -> L_S_RClss
-var lmits = {beg:0, nxt:5};
-RET = fn(lmits)(HTMLCol_Spans);//::D_lmits -> L_ -> L_S_RClss
+// //CAN return a sliced set of outerHTML Strings:: D_Lmits -> L_S_elements -> L_S_elements
+// RET = _RClss_S_outerHTML({beg:0, nxt:5})(_L_S_outerHTMLs(HTMLCol_Spans));
+// TST = RET.length;
+// EXP = 5;
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
 
-/**
- *  ---     --- TESTS ---
- */
-//CONFIRMED composed fn -> subset of innerHTML strings
+// //CONFIRMED _RClss_S_outerHTML(limits, spanStrings) -> subset of innerHTML strings
+// RET = RET[2];// L_Elems -> L_El_outerHTMLs[ndx] -> S_HTML_span
+// TST = R.slice(9,17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+
+
+// NOW compose the whole thing:: HTMLCollections -> fn(D_lmits) -> S_RClss_innerHTML
+var fn = d_lmits => R.compose(_RClss_S_outerHTML(d_lmits), _L_S_outerHTMLs);// D_lmits -> L_ -> S_RClss
+var lmits = {beg:0, nxt:5};
+RET = fn(lmits)(HTMLCol_Spans);
+
+//CONFIRMED _RClss_S_outerHTML(limits, spanStrings) -> subset of innerHTML strings
 RET = RET[2];// L_Elems -> L_El_outerHTMLs[ndx] -> S_HTML_span
 TST = R.slice(9,17, RET);
-EXP = '3 Yea, a';
+EXP = 'uuu3 Yea, a';
 t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
 
-// NOW merge?? the list into one RClss.outerHTML
+
 
 
 // hard coded current Node Lists
