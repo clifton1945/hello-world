abde9a2f2e611772f53480f208a7345c38c591e0
clif edwards <clif.edwards.2@gmail.com>
2016-08-12 11:44:24 -0400

    160812 @1143 -> SWITCHED TO straight Chptr_2_Rclss Directory.  copied the new_/ main and index and reset a fer references

--- Chptr_2_Rclss/main.js
+++ Chptr_2_Rclss/main.js
@@ -1,100 +1,132 @@
 /**
- *  main.js
- *  160808  @1810  R.lenses WORKS !!!
- *      @02040 -> WIP update_L_RClss_NLs(with a new current limits)
- *      TRANSFORMS the three Rclss span NodeLists.
- *      THEN the 3 Rclss div classes are reset using innerHTML.
- *  160803  @0830 -> APPLYING set_ElemCSD TO 4 Rclss divs IN 2Nephi31_.html.
- *  IN FILE: main.js -> SET each Verse CSD as a function of its Space parameters.
+ *  Chptr_2_Rclss/main.js -> set_one_DIV_RClss() UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden
+ *  160812  @1225 ->   set_one_DIV_RClss() WORKS READY FOR weight CSD AND set Eleme
+ *      @0930 REFACTFn Names and requires
+ *      @0512
+ *      -> ADDED the 3 RClss DIVs OUTSIDE of div.Chptr_31.
+ *      -> USED set_one_DIV_RClss() TO SET RClssDIV.innerHTML!!
+ *      <br
+ *      @0410   -> pulled get_LIST_of_outerHTML_STRs OUT OF set_one_DIV_RClss()
+ *  IN FILE: Chptr_2_Rclss/main.js -> set_one_DIV_RClss() UPDATES the 3 RClss DIVS.
+ *  They are now stand alone DIVS and the Chptr_31 DIV is :hidden
  */
 
 "use strict";
 // requires
 var R = require('ramda');
-var C_Both = require('./src/h').C_Both;
+var t = require('assert');
+var h = require('./src/h');
 
-
-// A TEST !!! 160809 @1840
-var tst_div = document.querySelector('.cur_div');// -> div.pst_div
-var RC_curLens = R.lensProp("innerHTML");
-var CUT = R.view(RC_curLens)(tst_div);// D_doc -> S
-var RET = R.set(RC_curLens, R.toLower(CUT))(tst_div);
-C_Both(RET.innerHTML); //HEY this works !!
-
+// var update_L_RClss_NLs = require('./src/update_L_RClss_NLs');//
+//var set_ElemStyle = require('../src/set_anElem_CSD');//
 
 /**
  *          ----- main helper functions -----
  */
-var update_L_RClss_NLs = require('./src/update_L_RClss_NLs');//
-var set_ElemStyle = require('./src/set_anElem_CSD');//
+var C_Both = h.C_Both;
+var mySlice = h._mySlice;
+/**
+ *      --- set_RClss_TO_::D_Lmits -> L_S_spans -> L_S_spans
+ * @param d_rcLmits
+ * @private
+ */
+var set_RClss_TO_ = h.set_RClss_TO_;
+/**
+ *      --- get_outerHTML_Str:: El -> S_outerHTML
+ * @param el
+ * @private
+ */
+var get_outerHTML_Str = h.get_outerHTML_Str;// E_a -> S_a
+/**
+ *      --- get_LIST_of_outerHTML_STRs:: L_Elems -> L_STR
+ *  @private
+ */
+const get_LIST_of_outerHTML_STRs = R.map(get_outerHTML_Str);// L_SPANS -> L_SPAN_outerHTML_STR
+
 //GLOBALS
-C_Both("IN  main.js.");
-var RET, EXP;
+C_Both("IN  new_/main.js.");
 
-// hard coded current Node Lists
-var aChptr_NL = document.querySelectorAll('span'); // -> NL:[span, span...]
-// default aChptr_NL Rclsses::  lengths 17+2+34->53
-
-/**
- * -------------  update the3 Rclss NodeLists: pst_, cur_, fut_NL
- */
-var stub_D_ndxLmits = {beg: 17, end: 22};// lengths will be :  17+5+31->53
-var [pst_NL, cur_NL, fut_NL ] =  update_L_RClss_NLs(aChptr_NL, stub_D_ndxLmits);// NL->D -> L{NL, NL, NL]
-// some tests
-RET = aChptr_NL.length;
-EXP = 53;
-assert.deepEqual(RET, EXP, `aChptr_NL.length IS:${RET}. EXP:${EXP}`);
-RET = pst_NL.length;
-EXP = 17;
-assert.deepEqual(RET, EXP, `pst_NL.length IS:${RET}. EXP:${EXP}`);
-RET = cur_NL.length;
-EXP = 5;
-assert.deepEqual(RET, EXP, `cur_NL.length IS:${RET}. EXP:${EXP}`);
-RET = fut_NL.length;
-EXP = 31;
-assert.deepEqual(RET, EXP, `fut_NL.length IS:${RET}. EXP:${EXP}`);
+// test data
+var RET, TST, EXP;
+// var stub_Chptr_DIV = document.querySelector('.Chptr_31');//:: div.Chptr_31
+// var stub_HTMLColl_of_SPANS = stub_Chptr_DIV.children;//-> HTMLCollection
+// var stub_one_Span = stub_HTMLColl_of_SPANS[2];
+// var stub_rclmits = {beg: 0, nxt: 5};
 
 /**
- * ----------- NEXT transform 3 Rclss NodeList into a new Chptr.innerHTML String -------
+ *  ---     --- TESTS ---
  */
+// // CAN return one Chptr SPAN as an outerHTML String:: El -> S
+// RET = get_outerHTML_Str(stub_one_Span);//D -> S
+// TST = R.slice(9, 17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+//
+// // CAN RETURN a List/Array of Chptr SPANs AS outerHTML Strings:: L_SPANS -> L_SPAN_outerHTML_STR
+// RET = get_LIST_of_outerHTML_STRs(stub_HTMLColl_of_SPANS);// L_Elems -> L_SPAN_STR
+//
+// // CONFIRM isArrayLike
+// t.ok(R.isArrayLike(RET));
+// // CONFIRM one of the list is a STR
+// t.ok(R.is(String, RET[0]));
+// //CONFIRM a list validity
+// RET = RET[2];//-> STR
+// TST = R.slice(9, 17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+// TEST: set_RClss_TO_
+// RET = set_RClss_TO_({beg: 0, nxt: 5})(get_LIST_of_outerHTML_STRs(stub_HTMLColl_of_SPANS));
+//
+// // CONFIRM isArrayLike
+// t.ok(R.isArrayLike(RET));
+// t.ok(R.is(String, RET[0]), 'this is NOT a STR');
+// //CONFIRMED
+// // RET -> List[2] IS Valid
+// RET = RET[2];// LOOK AT One_outerHTML_STR. RET -> List[2] IS Valid
+// TST = R.slice(9, 17, RET);
+// EXP = '3 Yea, a';
+// t.equal(TST, EXP, `EXP:["${EXP}"], NOT:["${TST}"]`);
+
+// /**
+//  *  --- LIST_reducedTO_one_DIV_outerHTML_STR:: [S,S, ...] -> S
+//  *  USE: reduces n SPAN.outerHTML Strings to one String.
+//  *  @private
+//  */
+// const LIST_reducedTO_one_DIV_outerHTML_STR = R.reduce(R.concat, '');
 
 /**
- *  --------------- SET the -> Rclss ..._divs ------------
- * @type {Element}
+ *  --- set_one_DIV_RClss:: L -> D -> S
+ *  set_one_DIV_RClss( D_RCss_NdxLmits) -> L_SPANS -> DIV_outerHTML_STR
+ * @param d_rcLmits
+ * @private
  */
-var pst_div = document.querySelector('.pst_div');// -> div.pst_div
+const set_one_DIV_RClss = d_rcLmits => R.compose(
+    R.reduce(R.concat, ''),
+    set_RClss_TO_(d_rcLmits)
+);
+
+//TEST:: set_oneDIV_RClss
+// var rclmits = {beg:0, nxt:1};
+
+// CONSTANTS
+var pst_div = document.querySelector('.pst_div');
 var cur_div = document.querySelector('.cur_div');
 var fut_div = document.querySelector('.fut_div');
+var LIST0 = document.querySelectorAll('.Chptr_31 span');
 
-/**
- * -------  REPLACE the 3 RClss div.innerHTML w/ new  S:innerHTML  -----------
- */
 
-var inner_cur0 = cur_div.innerHTML;//  TEST: -> SEE innerHTML -> S
+var LIST1 = get_LIST_of_outerHTML_STRs(LIST0); // KEEP SEPARATE it does not change much
 
-var pst_test = R.map(function (el) {
-    return '<span>' + el.innerHTML + '</span>';
-})(pst_NL);
-var cur_test = R.map(function (el) {
-    return '<span>' + el.innerHTML + '</span>';
-})(cur_NL);
-var fut_test = R.map(function (el) {
-    return '<span>' + el.innerHTML + '</span>';
-})(fut_NL);
+pst_div.innerHTML = set_one_DIV_RClss({beg:0, nxt:5})(LIST1);
+cur_div.innerHTML = set_one_DIV_RClss({beg:5, nxt:7})(LIST1);
+fut_div.innerHTML = set_one_DIV_RClss({beg:7, nxt:52})(LIST1);
 
-pst_div.innerHTML = R.reduce((a, b)=>a + b, '')(pst_test);// L -> S
-cur_div.innerHTML = R.reduce((a, b)=>a + b, '')(cur_test);
-fut_div.innerHTML = R.reduce((a, b)=>a + b, '')(fut_test);
-// tests:   see results
-var inner_cur1 = cur_div.innerHTML;// S -> S
-var aChptr_NL1 = document.querySelectorAll('span');// -> NL:[span, span...]
+// OK NOW and finally, set a RClssDIV.outerHTML BY HAND
 
-/**
- * --------------  Map all Spans with set_ElemStyle//:: (E:e, N:ndx_e, L:fam_e) ->  E: mutated
- */
-var myMap = R.addIndex(R.map);
-//noinspection JSUnusedLocalSymbols
-var out = myMap(set_ElemStyle)(aChptr_NL1);
 
-// END
-C_Both(JSON.stringify("OUT main.js."));
+C_Both("OUT new_/main.js.");
+
+// RET = set_one_DIV_RClss({beg:0, nxt:1})(LIST1);
+// t.ok(R.is(String, RET), 'DIV_RClss STR IS NOT a STR');
+// OK NOW set a RClssDIV.innerHTML BY HAND
+
