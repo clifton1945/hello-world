ae6514830220775ae7ed926c627f21ff0a5e1a67
clif edwards <clif.edwards.2@gmail.com>
2016-08-13 10:09:00 -0400

    *  main.js  *  160813  @1006 READY TO SET Elm.StyleObj  NOTE:  *  READ Setting style in >>https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style  *      @0923 -> moved set_RClss_Divs.js INTO bse Chptr_2_Rclss/src  *      @0715 -> REFACT name: L_Spans_TO_L_RclssSpans_BY_()  *  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden

--- Chptr_2_Rclss/main.js
+++ Chptr_2_Rclss/main.js
@@ -1,6 +1,8 @@
 /**
- *  new_/main.js
- *  160813  @0923 -> moved set_RClss_Divs.js INTO bse Chptr_2_Rclss/src
+ *  main.js
+ *  160813  @1006 READY TO SET Elm.StyleObj  NOTE:
+ *  READ Setting style in >>https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+ *      @0923 -> moved set_RClss_Divs.js INTO bse Chptr_2_Rclss/src
  *      @0715 -> REFACT name: L_Spans_TO_L_RclssSpans_BY_()
  *  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden
  */
@@ -20,19 +22,15 @@ var cur_div = document.querySelector('.cur_div');
 var fut_div = document.querySelector('.fut_div');
 var LIST0 = document.querySelectorAll('.Chptr_31 span');
 
-
-var LIST1 = _L_Span_outerHTML_Str(LIST0); // KEEP SEPARATE it does not change much
-
-pst_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:0, nxt:5})(LIST1);
-cur_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:5, nxt:7})(LIST1);
-fut_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:7, nxt:52})(LIST1);
-//
+var L_Span_outerHTML_Str = _L_Span_outerHTML_Str(LIST0); // KEEP SEPARATE it does not change much
 // // OK NOW and finally, set a RClssDIV.outerHTML BY HAND
-//
+pst_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:0, nxt:5})(L_Span_outerHTML_Str);
+cur_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:5, nxt:7})(L_Span_outerHTML_Str);
+fut_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:7, nxt:52})(L_Span_outerHTML_Str);
+
+// OK NOW set each Rclss Elm.Style
+
+// RET = L_RclssSpans_TO_one_Sof_Rclss_Spans_BY_({beg:0, nxt:1})(L_Span_outerHTML_Str);
+// t.ok(R.is(String, RET), 'DIV_RClss STR IS NOT a STR');
 
 C_Both("OUT new_/main.js.");
-
-// RET = L_RclssSpans_TO_one_Sof_Rclss_Spans_BY_({beg:0, nxt:1})(LIST1);
-// t.ok(R.is(String, RET), 'DIV_RClss STR IS NOT a STR');
-// OK NOW set a RClssDIV.innerHTML BY HAND
-
