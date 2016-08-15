c959df26f2c95effe0de5be7128c3d6187eaad67
clif edwards <clif.edwards.2@gmail.com>
2016-08-15 15:32:47 -0400

    *  main.js  *  160815  @1503  Basic Tests  *  @1050 PLAN over all: now available 3 Rclss divs OF spanStrs  *      then map(Fn, L_Spans)  *      where Fn_callBack: updateEachSpan(E, N, L):: -> E_updatedStyleObj  *      with pipeLike  *      N_ndx-> N_wt -> CSD_valu ->  *      where update Span -> Span_outerHTML_Str ->

--- Chptr_2_Rclss/main.js
+++ Chptr_2_Rclss/main.js
@@ -1,9 +1,12 @@
 /**
  *  main.js
- *  160813  @1006 READY TO SET Elm.StyleObj
- *  NOTE:   READ Setting style in >>https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
- *      @0923 -> moved set_RClss_Divs.js INTO bse Chptr_2_Rclss/src
- *      @0715 -> REFACT name: L_Spans_TO_L_RclssSpans_BY_()
+ *  160815  @1503  Basic Tests
+ *  @1050 PLAN over all: now available 3 Rclss divs OF spanStrs
+ *      then map(Fn, L_Spans)
+ *      where Fn_callBack: updateEachSpan(E, N, L):: -> E_updatedStyleObj
+ *      with pipeLike
+ *      N_ndx-> N_wt -> CSD_valu ->
+ *      where update Span -> Span_outerHTML_Str ->
  *  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden
  */
 
@@ -23,15 +26,25 @@ var _one_Sof_Rclss_Spans_BY_ = require('./src/set_RClss_Divs').L_RclssSpans_TO_o
 var pst_div = document.querySelector('.pst_div');
 var cur_div = document.querySelector('.cur_div');
 var fut_div = document.querySelector('.fut_div');
-var LIST0 = document.querySelectorAll('.Chptr_31 span');
 
-var L_Span_outerHTML_Str = _L_Span_outerHTML_Str(LIST0); // KEEP SEPARATE it does not change much
+var Chptr_Span_NL = document.querySelectorAll('.Chptr_31 span');
+var fut_NL = document.querySelectorAll('.fut_div');
+
+var L_Span_outerHTML_Str = _L_Span_outerHTML_Str(Chptr_Span_NL); // KEEP SEPARATE it does not change much
+
 // OK NOW and finally, set a RClssDIV.outerHTML BY HAND
 pst_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:0, nxt:5})(L_Span_outerHTML_Str);
 cur_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:5, nxt:7})(L_Span_outerHTML_Str);
 fut_div.innerHTML = _one_Sof_Rclss_Spans_BY_({beg:7, nxt:52})(L_Span_outerHTML_Str);
 
 // OK NOW set each Rclss Elm.Style
-t.ok(R.is(String, cur_div.innerHTML), 'cur_div IS an Object');
+//  BASIC TESTS: cur_div
+t.ok(cur_div.tagName == 'DIV', 'cur_div SHOULD BE a DIV.');
+t.ok(cur_div.firstElementChild.tagName == 'SPAN', 'cur_div.firstElementChild SHOULD BE a SPAN.');
+t.ok(R.isArrayLike( cur_div.children), 'cur_div-children SHOULD BE a ArrayLike.');
+t.ok(R.isArrayLike( fut_NL), 'fut_NL SHOULD BE a ArrayLike.');
+
+// some learning tests
+
 
 C_Both("OUT main.js");
