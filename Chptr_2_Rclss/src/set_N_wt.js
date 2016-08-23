7b94c6751406648738c84a70b5cd49ede497ff2c
clif edwards <clif.edwards.2@gmail.com>
2016-08-22 20:53:50 -0400

    * f_set_wtN:: (D, N)-> N -> N.  (D_csdLmtsL: N_elemFaml), N_elemNdx):: -> N_wt  *  160822  @2050   STABLE w/ TESTS ->  REFACT to just one function set_wtN/  * IN FILE: set_N_wt.js -> CALCULATE a weighting factor AS a function of a Verse Context/Space parameters:

--- Chptr_2_Rclss/src/set_N_wt.js
+++ Chptr_2_Rclss/src/set_N_wt.js
@@ -1,13 +1,7 @@
 /**
- * set_N_wt:: (D,L)->N -> N.  (D_csdLmtsL: L_elemFaml), N_elemNdx):: -> N_wt
- *  160816  @0530
- * 160815   1340  NAME CHANGE
- * 160730   1518 -> removed _n_calcWt()
- *          AND just recognized that the D_csdLmts ARE A FUNCTION of Rclss. I'll need REFACT then.
- *  @1224 -> quick test in main.js shows set_N_wt WORKS as a compose.
- *  @1000 -> RENAMED set_N_wt([D:csdSpan, L_fam]) -> N_elemNdx  --> N_wt
+ * f_set_wtN:: (D, N)-> N -> N.  (D_csdLmtsL: N_elemFaml), N_elemNdx):: -> N_wt
+ *  160822  @2050   STABLE w/ TESTS ->  REFACT to just one function set_wtN/
  * IN FILE: set_N_wt.js -> CALCULATE a weighting factor AS a function of a Verse Context/Space parameters:
- *      csd D_csdSpan, familyL, itsNdxN.
  */
 "use strict";
 let R = require('ramda');
@@ -30,36 +24,18 @@ let f_set_wtN = R.curry(function f_set_N_wt(D_csdSpan, N_famLen, N_ndx) {
 }); // D->N -> N -> N:wt
 
 /**
- *      f_set_N_wt(D_csdLmts: L_elemFaml, N_elemNdx):: -> N_wt
- *      USED: typically used to weight element property CSD: e.g. opacity, fontSize, etc
- *      typically the L:fam and N:elemNdx will be returned by indexedMaps.
- * @param D_csdSpan   -> {smlWt:a, lrgWt:a}   style Property beg and end limits
- * @param L_fam  -> [a] list of this elements family
- * @param N_ndx  -> a the index of this elem in the family lis
- * @returns {*} -> an Element weight for this context.
- * @private
+ *          ---------- EXPORTS ---------
  */
-let f_set_N_wt = R.curry(function f_set_N_wt(D_csdSpan, L_fam, N_ndx) {
-    var smlWt = D_csdSpan.smlWt;
-    var lrgWt = D_csdSpan.lrgWt;
-    var len = L_fam.length - 1;
-    return len > 0 ? -(lrgWt - smlWt) / len * N_ndx + lrgWt : lrgWt; // always lrgWt
-}); // D->L -> N -> N:wt
-
 /**
- *  set_N_wt([D_csdSpan, L_fam]) -> N_elemNdx -> N_wt
+ *  set_wtN([D_csdSpan, N_famLen) -> N_elemNdx -> N_wt
  * @param D_csdSpan
- * @param L_fam
+ * @param N_famLen
  * @private
  */
-const set_N_wt = R.partial(f_set_N_wt);//:: L:[D_csdSpan, L_fam] -> N_elemNdx -> N_wt
-
-// MODULES.EXPORT
-// set_N_wt([D_csdSpan, L_fam]):: N_elemNdx -> N_wt
-module.exports = {f_set_wtN, set_N_wt, f_set_N_wt};//::(D_csdLmits, L_elemFaml, N_elemNdx) -> N_wt
+exports.set_wtN = R.curry(f_set_wtN);//:: [D_csdSpan, L_fam] -> N_elemNdx -> N_wt
 
 /**
- *          ----------------  CODE UNDER TEST: set_N_wt:: N_ndx -> N_wt -----------
+ *          ----------------  CODE UNDER TEST: set_wtN:: N_ndx -> N_wt -----------
  * @type {{smlWt: number, lrgWt: number}}
  */
 var CUT, RET;
@@ -67,11 +43,11 @@ var CUT, RET;
 var csdLimitsD = {smlWt: 0.5, lrgWt: 0.9};
 var stub = [0, 1, 2, 3, 4, 5, 6];
 // CUT
-CUT = set_N_wt([csdLimitsD, stub]);
+CUT = R.curry(f_set_wtN(csdLimitsD, stub.length));// N -> N
 // ACCEPTABLE
 var assert = require("assert");
 assert.equal(CUT(0), 0.9, 'exp: 0.9 FROM assert set_N_wt(0)');
 assert.equal(CUT(6), 0.5, 'exp: 0.5 FROM assert set_N_wt(6)');
-assert.equal(f_set_N_wt({smlWt: 0.5, lrgWt: 0.9}, stub, 4), 0.6333333333333333, 'exp: 0.633... FROM assert set_N_wt(4)');
+assert.equal(f_set_wtN({smlWt: 0.5, lrgWt: 0.9}, stub.length, 4), 0.6333333333333333, 'exp: 0.633... FROM assert set_N_wt(4)');
 // NOT ACCEPTABLE
 assert.notEqual(CUT(), 0.9, 'FAIL_with NO argument set_N_wt()');
