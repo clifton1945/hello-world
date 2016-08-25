ef7c5a360ab8562ad785d5a0fe64efc8bbeb9976
clif edwards <clif.edwards.2@gmail.com>
2016-08-25 12:31:53 -0400

    *  main.js  *  160825  @1230   STABLE WIP hardcoded -> USING set_RclssDIVs.js/set_RclssDIVs()  *  160824  @0715 -> WIP ADD key events for Reading next verse  *      -> RENAMED DEPR back to './src/set_RClss_Divs').L_Spans_TO_L_Span_outerHTML_Str  *  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden  */

--- Chptr_2_Rclss/main.js
+++ Chptr_2_Rclss/main.js
@@ -1,15 +1,12 @@
 /**
  *  main.js
+ *  160825  @1230   STABLE WIP hardcoded -> USING set_RclssDIVs.js/set_RclssDIVs()
  *  160824  @0715 -> WIP ADD key events for Reading next verse
  *      -> RENAMED DEPR back to './src/set_RClss_Divs').L_Spans_TO_L_Span_outerHTML_Str
- *  160823  @1230 _> FIX: './src/DEPR_set_RClss_Divs').L_Spans_TO_L_Span_outerHTML_Str
- *      @1225 WHY see 2 IN main.js
- *      @1155 -> WIP STABLE USING REFACTED ->  set_anElem_Style.set_SPAN_Style()
-*  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden
+ *  IN FILE: -> UPDATES the 3 RClss DIVS. They are now stand alone DIVS and the Chptr_31 DIV is :hidden
  */
-
+//
 "use strict";
-// requires♣
 var h = require('./src/h');
 var C_Both = h.C_Both;
 C_Both("IN  main.js");
@@ -22,24 +19,27 @@ var t = require('assert');
  */
 var set_SPAN_Style = R.curry(require('./src/set_anElem_Style').set_SPAN_Style);//
 var _outerHTML_SPAN_L = require('./src/set_RClss_Divs').L_Spans_TO_L_Span_outerHTML_Str;
+// CUT
+var set_RclssDIVs = require('./src/set_RclssDIVs').set_RclssDIVs;// (N_cur, L_spans) -> N_curSpans -> L:[[pst],[cur],[fut]]
 
 // -> Rclss DIV DEFINITIONS set here SO THAT the CSS styles are in effect for the DIV
 var pst_div = document.querySelector('.pst_div');// -> div.pst_div
 var cur_div = document.querySelector('.cur_div');
 var fut_div = document.querySelector('.fut_div');
-// CONSTANTS
+// CONSTANT Lists of Spans
 var SPAN_NL = document.querySelectorAll('.Chptr_31 span');// -> NL[52]:: [span, spam, ...]
 var outerHTML_SPAN_L = _outerHTML_SPAN_L(SPAN_NL);// -> Array[52]:: [S:"<span> 1 And...", S, ...]
-
-var f_readDict = (clssLenN) => {return {pstBegN:0, pstEndN: 10, curEndN: 15, futEndN:clssLenN}};
 
 /**
  * ----------------- Visual Confirm w/ index.html::  setting Rclss Element Styles ---------------------------
  */
+// here is a list of the three Rclss set from set_RclssDIVs
+var _L_Rclss = set_RclssDIVs(2, outerHTML_SPAN_L);// partialed. N_page  -> L_
+var L_Rclss = _L_Rclss(15);
 // THESE 3 lines set the 3 Rclss children
-pst_div.innerHTML = R.slice(0, 10)(outerHTML_SPAN_L); // -> S[2617]:"<spam 1 And.....
-cur_div.innerHTML = R.slice(10, 15)(outerHTML_SPAN_L);
-fut_div.innerHTML = R.slice(15, 52)(outerHTML_SPAN_L);
+pst_div.innerHTML = L_Rclss[0]; // -> S[2617]:"<spam 1 And.....
+cur_div.innerHTML = L_Rclss[1];
+fut_div.innerHTML = L_Rclss[2];
 
 /**
  *  ----------------- WIP ON pst_div VISUAL TESTS-------------------------
@@ -69,4 +69,4 @@ CUT = set_SPAN_Style(rclss_wtRng, rclss_fmlyLen);
 RET = R.addIndex(R.map)(CUT)(L);//Elem,N,L -> [Elem,...]
 
 C_Both("OUT main.js");
-C_Both("LOOK AT TODOS");
+// C_Both("LOOK AT TODOS");
